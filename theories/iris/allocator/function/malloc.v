31cafc5e824081d680239d3a8782954d728b539d
Ryan Doenges <me@ryandoeng.es>
2025-04-10 21:19:46 -0400

    implement proper malloc

--- theories/iris/allocator/function/malloc.v
+++ theories/iris/allocator/function/malloc.v
@@ -30,11 +30,11 @@ This memory is partitioned into a circular linked list of blocks.
 
 enum state_t ::= FREE | USED | FINAL
 
-struct block {
+struct block_t {
   state_t   state;
   i32       size; // must be nonzero
   i32       next;
-  i32[size] data; // user data
+  u8[size]  data; // user data
 }
 
 INITIAL STATE
@@ -50,19 +50,20 @@ The last block is the unique block marked FINAL and never contains user data.
 
 GROWING THE MEMORY
 pinch_block(final_block, reqd_sz):
-  new_size = final_block.size - reqd_sz
-  new_block = final_block + 12 + reqd_sz
+  new_total_sz = reqd_sz + 12
+  new_block = final_block + new_total_sz
+
+  new_block.state = FINAL
+  new_block.size = final_block.size - new_total_sz
+  new_block.next = 0
   final_block.state = FREE
   final_block.size = reqd_sz
   final_block.next = new_block
-  new_block.state = FINAL
-  new_block.size = new_size
-  new_block.next = 0
-  return new_block
+  return final_block
 
 new_block(final_block, reqd_sz):
-  if final_block.sz > reqd_sz + ???:
-    return split_block(final_block, reqd_sz)
+  if final_block.sz > reqd_sz + 12:
+    return pinch_block(final_block, reqd_sz)
   else:
     final_block.state = FREE
     final_block.next = mem_size
@@ -70,7 +71,7 @@ new_block(final_block, reqd_sz):
     new_block = final_block.next
     new_block.state = FINAL
     new_block.size = actual_sz
-    return split_block(new_block, reqd_sz)
+    return pinch_block(new_block, reqd_sz)
 
 MALLOC
 malloc(reqd_sz):
@@ -100,131 +101,232 @@ free(addr):
   // address wasn't the address of a known allocation
   trap
 
-
-
-
-
-
-A block contains some metadata and then the data (the memory given to the client).
-[ metadata ] [ data .... ]
-
-The metadata includs
-
-The free list is a linked list of blocks.
-The terminal block has -1 where its next pointer should be.
-[b1] --> [b2] --> [b3] --> -1
-
 *)
   
-Definition BLK_FREE : N := 0.
-Definition BLK_USED : N := 1.
-Definition BLK_STOP : N := 2.
+Definition BLK_FREE  : N := 0.
+Definition BLK_USED  : N := 1.
+Definition BLK_FINAL : N := 2.
 
-(* Given a pointer to a block in the free list, return the block's state marker. *)
-Definition get_block_state :=
+(* sizeof(state_t) *)
+Definition state_sz : N := 4.
+(* sizeof(block_t) sans data *)
+Definition blk_hdr_sz : N := 12.
+
+(* offsets for fields of block_t *)
+Definition state_off : N := 0.
+Definition size_off  : N := 4.
+Definition next_off  : N := 8.
+Definition data_off  : N := blk_hdr_sz.
+
+Definition get_state blk :=
+  BI_get_local blk ::
+  BI_load T_i32 None state_off 0%N ::
+  nil.
+
+Definition get_next blk :=
+  BI_get_local blk ::
+  BI_load T_i32 None next_off 0%N ::
+  nil.
+
+Definition get_data blk :=
+  BI_get_local blk ::
+  BI_load T_i32 None data_off 0%N ::
+  nil.
+
+Definition set_next :=
+  [BI_store T_i32 None next_off 0%N].
+
+(* this is the size of the data segment of the block *)
+Definition get_size blk :=
+  BI_get_local blk ::
+  BI_load T_i32 None size_off 0%N ::
+  nil.
+  
+Definition set_size :=
+  [BI_store T_i32 None size_off 0%N].
+
+Definition add_hdr_sz :=
+  u32const blk_hdr_sz ::
+  BI_binop T_i32 (Binop_i BOI_add) ::
+  nil.
+
+(* this is the size of the whole block, including header! *)
+Definition get_total_size blk :=
+  get_size blk ++
+  add_hdr_sz.
+
+Definition mark_free blk :=
   [
-    BI_load T_i32 None 0%N 0%N
-  ].
-
-Definition get_block_stop := 
-  get_block_state ++
-  [u32const BLK_STOP;
-   BI_relop T_i32 (Relop_i ROI_eq)].
-
-Definition get_block_free := 
-  get_block_state ++
-  [u32const BLK_FREE;
-   BI_relop T_i32 (Relop_i ROI_eq)].
-
-Definition get_block_used := 
-  get_block_state ++
-  [u32const BLK_USED;
-   BI_relop T_i32 (Relop_i ROI_eq)].
-
-Definition mark_block_free :=
-  [
+    BI_get_local blk;
     u32const BLK_FREE;
     BI_store T_i32 None 0%N 0%N
   ].
 
-Definition mark_block_used :=
+Definition mark_used blk :=
   [
+    BI_get_local blk;
     u32const BLK_USED;
     BI_store T_i32 None 0%N 0%N
   ].
 
-
-Definition get_block_size :=
+Definition mark_final blk :=
   [
-    BI_load T_i32 None 4%N 0%N
+    BI_get_local blk;
+    u32const BLK_FINAL;
+    BI_store T_i32 None 0%N 0%N
   ].
+
+Definition mem_size :=
+  [
+    BI_current_memory;
+    u32const Wasm.operations.page_size;
+    BI_binop T_i32 (Binop_i BOI_mul)
+  ].
+
+Definition is_block_nonfinal blk :=
+  get_state blk ++
+  u32const BLK_FINAL ::
+  BI_relop T_i32 (Relop_i ROI_ne) ::
+  nil.
+
+Definition is_block_free blk :=
+  get_state blk ++
+  u32const BLK_FREE ::
+  BI_relop T_i32 (Relop_i ROI_eq) ::
+  nil.
+
+(*
+  pinch_block final sz 
+  locals declared: [i32]
+
+  Take a final block of size M
+    [hdr| M...                  ]
+  and pinch off a new one of a given size N < M - 12. The leftovers become
+  the new final block.
+    [hdr| N ][hdr| ...          ]
+  Returns the address of the new block.
+
+  Parameters/Locals:
+  0     rw, pointer to the final block.
+  1     ro, requested size to pinch off.
+  2     rw, storing the total size (incl headers) of the pinched block.
+  3     rw, storing the address of the new final block.
+*)
+Definition pinch_block final_block reqd_sz total_sz new_block :=
+  (* compute and save total size of reqd block *)
+  (BI_get_local reqd_sz ::
+   add_hdr_sz ++
+   BI_set_local total_sz ::
+   nil) ++ 
+  (* compute address of new final block *)
+  (BI_get_local final_block ::
+   BI_get_local total_sz ::
+   BI_binop T_i32 (Binop_i BOI_add) ::
+   BI_set_local new_block ::
+   nil) ++
+  (* set up new final block's header *)
+  mark_final new_block ++
+  (* set new block size *)
+  (BI_get_local new_block ::
+   (* compute block size on top of stack *)
+   get_size final_block ++
+   BI_get_local reqd_sz :: 
+   BI_binop T_i32 (Binop_i BOI_sub) ::
+   (* write top of stack to $3.size *)
+   set_size) ++
+  (* new_block.next = 0 *)
+  (u32const 0%N :: set_next) ++
+  (* set up pinched block *)
+  (mark_free final_block ++
+   BI_get_local final_block ::
+   BI_get_local reqd_sz ::
+   set_size ++
+   BI_get_local final_block ::
+   BI_get_local new_block ::
+   set_next).
+
+(*
+  new_block: [i32; i32] -> [i32]
+  Returns address of a block at least the requested size
+  Parameters/Locals:
+  0     parameter, pointer to final_block
+  1     parameter, requested size
+  2     local, actual size allocated
+*)
+Definition new_block final_block reqd_sz total_sz new_block actual_size :=
+  BI_get_local reqd_sz ::
+  add_hdr_sz ++
+  get_size final_block ++
+  BI_relop T_i32 (Relop_i (ROI_lt SX_U)) ::
+  BI_if (Tf [] [])
+    (* then *)
+    (pinch_block final_block reqd_sz total_sz new_block)
+    (* else *)
+    (mark_free final_block ++
+
+     BI_get_local final_block ::
+     BI_get_local reqd_sz ::
+     u32const Wasm.operations.page_size ::
+     BI_binop T_i32 (Binop_i (BOI_div SX_U)) ::
+     u32const 1%N ::
+     BI_tee_local actual_size ::
+     BI_binop T_i32 (Binop_i BOI_add) ::
+     BI_grow_memory ::
+     set_next ++
+
+     BI_get_local final_block ::
+     BI_get_local actual_size ::
+     u32const Wasm.operations.page_size ::
+     BI_binop T_i32 (Binop_i BOI_mul) ::
+     set_size ++
+     pinch_block final_block reqd_sz total_sz new_block) ::
+    nil.
+
+
+Definition malloc_loop_body reqd_sz cur_block :=
+  (* break out of the loop if the block is final. *)
+  is_block_nonfinal cur_block ++
+  BI_br_if 1 ::
+  (* Check if the block fits and is free. *)
+  (* put the free flag at the top of the stack*)
+  is_block_free cur_block ++
+  (* Put a boolean representing whether the size is big enough at the top of the stack *)
+  BI_get_local reqd_sz ::
+  get_size cur_block ++ 
+  BI_relop T_i32 (Relop_i (ROI_le SX_U)) ::
+  (* Compute free && fits *)
+  BI_binop T_i32 (Binop_i BOI_and) ::
+  [BI_if (Tf [] []) 
+    (* it's free and it fits, mark block as not free and return start *)
+    (mark_used cur_block ++
+     get_data cur_block ++ 
+     [BI_return]) 
+    (* if it isn't free or doesn't fit, try the next block *)
+    (get_next cur_block ++
+     [BI_set_local cur_block])]
+  .
+
+Definition malloc_body reqd_sz cur_block tmp1 tmp2 tmp3 :=
+  (* Location of first block in the free list. *)
+  i32const 0 ::
+  (* Store the current block. *)
+  BI_set_local cur_block ::
+  BI_loop (Tf [] []) (malloc_loop_body reqd_sz cur_block) ::
+  (* OK, we are at the end of the list! *)
+  new_block cur_block reqd_sz tmp1 tmp2 tmp3 ++
+  mark_used cur_block ++
+  BI_get_local cur_block ::
+  BI_return ::
+  nil.
   
-Definition set_block_size :=
-  [
-    BI_store T_i32 None 4%N 0%N
-  ].
-  
-Definition get_block_next :=
-  [
-    BI_load T_i32 None 8%N 0%N
-  ].
-
-Definition get_block_start :=
- [
-   i32const 12;
-   BI_binop T_i32 (Binop_i BOI_add)
- ].
-
 (*
   malloc: [i32] -> [i32]
   locals declared: [i32]
 
   Allocate a new block of memory of the requested size.
-
-  Parameters/Locals:
-  0     local variable, storing the requested size.
-  1     local variable, storing the current candidate block of the free list.
 *)
 Definition malloc :=
-  [
-    (* Location of first block in the free list. *)
-    i32const 0;
-    (* Store the current block in $1. *)
-    BI_set_local 1
-  ] ++ [
-    (* Loop invariant: $1 is either -1 or it points to a valid block. *)
-    BI_loop (Tf [] []) (
-      (* Check if the block is a STOP marker. *)
-      BI_get_local 1 ::
-      get_block_stop ++ [
-      BI_if (Tf [] [])
-          (* put the free flag at the top of the stack*)
-          (BI_get_local 1 ::
-           get_block_free ++
-           (* Put a boolean representing whether the size is big enough at the top of the stack *)
-           BI_get_local 0 ::
-           BI_get_local 1 ::
-           get_block_size ++ 
-           [BI_relop T_i32 (Relop_i (ROI_le SX_U));
-            (* Compute free && fits *)
-            BI_binop T_i32 (Binop_i BOI_and);
-            BI_if (Tf [] []) (
-                (* it's free and it fits, mark block as not free and return start *)
-                BI_get_local 1 ::
-                mark_block_used ++
-                BI_get_local 1 ::
-                get_block_start ++ 
-                [BI_return]
-            ) 
-            (get_block_next ++ [BI_set_local 1])
-          ]) [
-          (* address of block was null: trap *)
-          (* grow the free list *)
-          (* ultimately, decide we're out of space & return -1 *)
-          BI_unreachable
-      ]
-    ]) 
-  ].
+  malloc_body 0 1 2 3.
 
 End code.
 
@@ -257,20 +359,22 @@ Proof.
   eapply wp_label_push; auto.
 Qed.
 
-Inductive free_flag :=
+Inductive state_flag :=
 | Used
-| Free.
+| Free
+| Final.
 
-Definition flag_repr (flag : free_flag) : value :=
+Definition state_repr (flag : state_flag) : value :=
   match flag with
   | Used => value_of_uint BLK_USED
   | Free => value_of_uint BLK_FREE
+  | Final => value_of_uint BLK_FINAL
   end.
 
 Inductive block :=
-| BlkStop: block
+| BlkFinal: block
 | BlkGo: 
-  free_flag -> 
+  state_flag -> 
   i32 (* size *) ->
   block (* next *) ->
   block.
@@ -281,26 +385,27 @@ Definition nat_repr (n: nat) (m: i32) : Prop :=
 Definition own_block (memidx: N) (base_addr: N) (sz: i32) : iProp Σ :=
   ∃ bs: bytes, ⌜nat_repr (length bs) sz⌝ ∗ memidx ↦[wms][base_addr] bs.
 
-Definition data_repr (flag: free_flag) (sz: i32) (memidx: N) (base_addr: N) : iProp Σ :=
+Definition data_repr (flag: state_flag) (sz: i32) (memidx: N) (base_addr: N) : iProp Σ :=
   match flag with
   | Used => ⌜True⌝
   | Free => own_block memidx base_addr sz
+  | Final => ⌜True⌝
   end%I.
 
 (* The block representation invariant *)
 Fixpoint blk_rep blk memidx base_addr : iProp Σ :=
   match blk with
-  | BlkStop =>
-      memidx ↦[wms][base_addr] bits (value_of_uint BLK_STOP)
+  | BlkFinal => ⌜True⌝
   | BlkGo flag sz next =>
       ∃ next_addr,
-        memidx ↦[wms][base_addr] bits (flag_repr flag) ∗
+        memidx ↦[wms][base_addr] bits (state_repr flag) ∗
         memidx ↦[wms][base_addr + 4] bits (VAL_int32 sz) ∗
         memidx ↦[wms][base_addr + 8] bits (value_of_uint next_addr) ∗
         data_repr flag sz memidx (base_addr + 12)%N ∗
         blk_rep next memidx next_addr
   end%I.
 
+(* Keeping these but commenting out since I broke the proofs
 Lemma spec_mark_block_used E f memidx blk sz blk_addr :
   ⊢ {{{{ blk_rep (BlkGo Free sz blk) memidx blk_addr ∗
          ↪[frame] f }}}}
@@ -372,6 +477,7 @@ Proof.
   iApply wp_seq; cbn.
 Abort.
 
+*)
 End specs.    
 End malloc.    
       
