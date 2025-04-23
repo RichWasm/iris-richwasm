794b50ce18534c3a5d126a971fe5ac0b1c901109
Brianna Marshall <brianna@bmarsha.com>
2025-04-22 22:23:43 -0400

    Define phyv

--- theories/iris/gclogic.v
+++ theories/iris/gclogic.v
@@ -30,17 +30,27 @@ Definition word := (byte * byte * byte * byte)%type.
 Record logptr := LogPtr { head : loc; offset : Z }.
 
 Inductive logval :=
-  | logptrV : logptr -> logval
   | wordV : word -> logval
-.
+  | logptrV : logptr -> logval.
 
 Definition logheap := gmap (loc * N) logval.
 
-(*Definition table := gmap loc (ptr * positive).*)
-Definition table := gmap loc ptr.
+Definition table := gmap loc (ptr * positive).
 
-Definition phyv (T : table) (v : logval) : word.
-Admitted.
+Definition phyv (T : table) (v : logval) : option word :=
+  match v with
+  | wordV w => Some w
+  | logptrV l =>
+      match gmap_lookup l.(head) T with
+      | None => None
+      | Some (p, sz) =>
+          if (0 <=? l.(offset))%Z && (l.(offset) <? Zpos sz)%Z then
+            let p' := Wasm_int.Int32.repr (N.to_nat p + Z.to_nat l.(offset)) in
+            let bs := serialise_i32 p' in
+            Some (seq.nth #00 bs 0, seq.nth #00 bs 1, seq.nth #00 bs 2, seq.nth #00 bs 3)%byte
+          else None
+      end
+  end.
 
 Definition word_to_bytes (w : word) : list byte :=
   let '(b1, b2, b3, b4) := w in [b1; b2; b3; b4].
@@ -57,15 +67,17 @@ Context `{!wasmG Σ, !gcwasmG Σ, !logrel_na_invs Σ}.
 
 Definition gcmem := N.of_nat 0.
 
-Notation "l ↦[gc] v" := (pointsto (L:=loc*N) (V:=logval) l (DfracOwn 1) v% V)
-  (at level 20, format "l ↦[gc] v") : bi_scope.
+Notation "l ↦[gc] v" := (pointsto (L:=loc*N) (V:=logval) l (DfracOwn 1) v%V)
+  (at level 20) : bi_scope.
 
-Notation " ↪[table] v" := (ghost_map_elem tableGName tt (DfracOwn 1) v%V)
-  (at level 20, format " ↪[table] v").
+(* ghost_map_elem is a fragment. GC module should use ghost_map_auth. *)
+Notation "↪[table] v" := (ghost_map_elem tableGName tt (DfracOwn 1) v%V)
+  (at level 20).
 
 Definition gc_inv : iProp Σ :=
-  ∃ (T : table),
-  ↪[table] T ∗
-  [∗ map] l↦p ∈ T, ∀ v, (l, N.of_nat 0) ↦[gc] v ∗-∗ gcmem ↦[wms][ p ] (word_to_bytes (phyv T v)).
+  ∃ T, ↪[table] T ∗
+  [∗ map] l↦ps ∈ T, ∀ v,
+  ∃ w, ⌜phyv T v = None \/ phyv T v = Some w⌝ ∗
+  (l, N.of_nat 0) ↦[gc] v ∗-∗ gcmem ↦[wms][ps.1] word_to_bytes w.
 
 End defs.
