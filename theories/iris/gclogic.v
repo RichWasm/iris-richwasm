774be1a30e513fe7b07a75c7259f3c1e12ac2ca9
Brianna Marshall <brianna@bmarsha.com>
2025-04-27 11:53:03 -0400

    Fix a single GC memory and use 32-bit words

--- theories/iris/gclogic.v
+++ theories/iris/gclogic.v
@@ -22,25 +22,27 @@ Record vblock := {
 
 Notation vstore := (gmap vloc vblock).
 
-Notation addr := (N * N)%type (only parsing).
+Notation addr := N (only parsing).
 
 Notation addr_map := (gmap vloc addr).
 
-Definition code_int (z : Z) : value :=
-  VAL_int64 (Wasm_int.int_of_Z i64m (2 * z + 1)).
+Notation word := (eqtype.Equality.sort i32).
 
-Definition code_addr '((m, i) : addr) : value :=
-  let to_int64 := Wasm_int.int_of_Z i64m in
-  let low := to_int64 (Z.of_N i) in
-  let high := Wasm_int.Int64.shl (to_int64 (Z.of_N m)) (to_int64 (Z.of_nat 32)) in
-  VAL_int64 (Wasm_int.Int64.add low high).
+Definition code_int (z : Z) : word :=
+  Wasm_int.int_of_Z i32m (2 * z + 1).
 
-Inductive repr_vval : addr_map -> vval -> value -> Prop :=
+Definition code_addr (a : addr) : word :=
+  Wasm_int.int_of_Z i32m (Z.of_N a).
+
+Inductive repr_vval : addr_map -> vval -> word -> Prop :=
   | repr_vint θ z :
       repr_vval θ (Vint z) (code_int z)
   | repr_vloc θ ℓ a :
       θ !! ℓ = Some a ->
       repr_vval θ (Vloc ℓ) (code_addr a).
+
+Definition serialize_word (w : word) : bytes :=
+  serialise_i32 w.
 
 (* TODO *)
 Definition serialize_vblock (blk : vblock) : bytes :=
@@ -50,15 +52,15 @@ Definition roots_are_live (θ : addr_map) (roots : gmap addr vval) : Prop :=
   ∀ a ℓ, roots !! a = Some (Vloc ℓ) -> ℓ ∈ dom θ.
 
 Definition gmap_inj `{Countable K} {V} (m : gmap K V) :=
-  ∀ k1 k2 v, m !! k1 = Some v → m !! k2 = Some v → k1 = k2.
+  ∀ k1 k2 v, m !! k1 = Some v -> m !! k2 = Some v -> k1 = k2.
 
 Definition GC_correct (ζ : vstore) (θ : addr_map) : Prop :=
-  gmap_inj θ ∧
+  gmap_inj θ /\
   ∀ ℓ blk ℓ',
-    ℓ ∈ dom θ ->
-    ζ !! ℓ = Some blk ->
-    Vloc ℓ' ∈ blk.(vals) ->
-    ℓ' ∈ dom θ.
+  ℓ ∈ dom θ ->
+  ζ !! ℓ = Some blk ->
+  Vloc ℓ' ∈ blk.(vals) ->
+  ℓ' ∈ dom θ.
 
 Class rwasm_gcG Σ := Rwasm_gcG {
   gcG_vstoreG :: ghost_mapG Σ vloc vblock;
@@ -73,12 +75,12 @@ Context `{wasmG Σ}.
 Context `{rwasm_gcG Σ}.
 
 Definition GC (θ : addr_map) : iProp Σ :=
-  ∃ (ζ : vstore) (roots : gmap addr vval),
-    ghost_map_auth gcG_vstore 1 ζ ∗
-    ghost_map_auth gcG_roots 1 roots ∗
-    ([∗ map] ℓ ↦ '(m, i); blk ∈ θ; ζ, m ↦[wms][i] serialize_vblock blk) ∗
-    ([∗ map] '(m, i) ↦ u ∈ roots, ∃ bs v, m ↦[wms][i] bs ∗ ⌜bs = bits v⌝ ∗ ⌜repr_vval θ u v⌝) ∗
-    ⌜roots_are_live θ roots⌝ ∗
-    ⌜GC_correct ζ θ⌝.
+  ∃ (ζ : vstore) (roots : gmap addr vval) (mem : N),
+  ghost_map_auth gcG_vstore 1 ζ ∗
+  ghost_map_auth gcG_roots 1 roots ∗
+  ([∗ map] ℓ ↦ a; blk ∈ θ; ζ, mem ↦[wms][a] serialize_vblock blk) ∗
+  ([∗ map] a ↦ u ∈ roots, ∃ bs w, mem ↦[wms][a] bs ∗ ⌜bs = serialize_word w⌝ ∗ ⌜repr_vval θ u w⌝) ∗
+  ⌜roots_are_live θ roots⌝ ∗
+  ⌜GC_correct ζ θ⌝.
 
 End GCtoken.
