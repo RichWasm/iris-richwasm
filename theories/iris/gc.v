4a492da15ba501c51980b1723c2001dfff52e44c
Brianna Marshall <brianna@bmarsha.com>
2025-04-28 00:04:07 -0400

    State wp_load_gc lemma and fix definitions

--- theories/iris/gc.v
+++ theories/iris/gc.v
@@ -1,3 +1,4 @@
+From iris.proofmode Require Import base tactics classes.
 From Wasm.iris.rules Require Import iris_rules.
 
 Notation vloc := nat (only parsing).
@@ -54,7 +55,10 @@ Inductive repr_vblock : addr_map -> vblock -> bytes -> Prop :=
       tag_b = serialize_vblock_tag blk.(tag) ->
       length_bs = serialize_nat_32 (length blk.(vals)) ->
       data_bs = flat_map serialize_word ws ->
-      repr_vblock θ blk (tag_b :: take 31 length_bs ++ data_bs).
+      repr_vblock θ blk (tag_b :: take 3 length_bs ++ data_bs).
+
+Definition vblock_offset (i : nat) : static_offset :=
+  N.of_nat (4 + 4 * i).
 
 Definition roots_are_live (θ : addr_map) (roots : gmap addr vval) : Prop :=
   ∀ a ℓ, roots !! a = Some (Vloc ℓ) -> ℓ ∈ dom θ.
@@ -80,25 +84,59 @@ Class rwasm_gcG Σ := Rwasm_gcG {
 Notation "ℓ ↦vblk{ dq } b" := (ℓ ↪[gcG_vstore]{dq} b)%I
   (at level 20, format "ℓ ↦vblk{ dq } b") : bi_scope.
 Notation "ℓ ↦vblk b" := (ℓ ↪[gcG_vstore] b)%I
-  (at level 20, format "ℓ ↦vblk b") : bi_scope.
+  (at level 20, format "ℓ  ↦vblk  b") : bi_scope.
 
 Notation "a ↦root{ dq } u" := (a ↪[gcG_roots]{dq} u)%I
   (at level 20, format "a ↦root{ dq } u") : bi_scope.
 Notation "a ↦root u" := (a ↪[gcG_roots] u)%I
-  (at level 20, format "a ↦root u") : bi_scope.
+  (at level 20, format "a  ↦root  u") : bi_scope.
 
 Section GCtoken.
 
 Context `{wasmG Σ}.
 Context `{rwasm_gcG Σ}.
 
-Definition GC (θ : addr_map) : iProp Σ :=
-  ∃ (ζ : vstore) (roots : gmap addr vval) (mem : N),
+Definition GC (m : memaddr) (θ : addr_map) : iProp Σ :=
+  ∃ (ζ : vstore) (roots : gmap addr vval),
   ghost_map_auth gcG_vstore 1 ζ ∗
   ghost_map_auth gcG_roots 1 roots ∗
-  ([∗ map] ℓ ↦ a; blk ∈ θ; ζ, ∃ bs, mem ↦[wms][a] bs ∗ ⌜repr_vblock θ blk bs⌝) ∗
-  ([∗ map] a ↦ u ∈ roots, ∃ bs w, mem ↦[wms][a] bs ∗ ⌜bs = serialize_word w⌝ ∗ ⌜repr_vval θ u w⌝) ∗
+  ([∗ map] ℓ ↦ a; blk ∈ θ; ζ, ∃ bs, N.of_nat m ↦[wms][a] bs ∗ ⌜repr_vblock θ blk bs⌝) ∗
+  ([∗ map] a ↦ u ∈ roots, ∃ bs w, N.of_nat m ↦[wms][a] bs ∗ ⌜bs = serialize_word w⌝ ∗ ⌜repr_vval θ u w⌝) ∗
   ⌜roots_are_live θ roots⌝ ∗
   ⌜GC_correct ζ θ⌝.
 
 End GCtoken.
+
+Section GCrules.
+
+Context `{wasmG Σ}.
+Context `{rwasm_gcG Σ}.
+
+Lemma wp_load_gc
+    (Φ : iris.val -> iProp Σ) (s : stuckness) (E : coPset)
+    (F : frame) (m : memaddr)
+    (k : eqtype.Equality.sort i32) (off : static_offset)
+    (i : nat) (w : word) (u : vval)
+    (θ : addr_map) (ℓ : vloc) (blk : vblock) :
+  F.(f_inst).(inst_memory) !! 0 = Some m ->
+  repr_vval θ (Vloc ℓ) k ->
+  blk.(vals) !! i = Some u ->
+  vblock_offset i = off ->
+  ▷ Φ (immV [VAL_int32 w]) ∗
+  ⌜repr_vval θ u w⌝ ∗
+  GC m θ ∗
+  ℓ ↦vblk blk ∗
+  ↪[frame] F ⊢
+  WP [AI_basic (BI_const (VAL_int32 k));
+      AI_basic (BI_load T_i32 None N.zero off)]
+     @ s; E
+     {{ w, Φ w ∗ GC m θ ∗ ℓ ↦vblk blk ∗ ↪[frame] F }}.
+Proof.
+  iIntros (Em Hk Eu Hoff) "(HΦ & %Hw & HGC & Hℓ & HF)".
+  iDestruct "HGC" as (ζ roots) "(Hζ & Hroots & Hζmem & Hrootsmem & %Hlive & %GCOK)".
+  inversion Hk as [|θ' ℓ' a Eθℓ Eθ' Eℓ' Ek]. subst.
+  iDestruct (ghost_map_lookup with "Hζ Hℓ") as "%Eζℓ".
+  iDestruct (big_sepM2_lookup_acc _ _ _ _ _ _ Eθℓ Eζℓ with "Hζmem") as "[(%bs & Ha & %Hbs) Hζmem]".
+Admitted.
+
+End GCrules.
