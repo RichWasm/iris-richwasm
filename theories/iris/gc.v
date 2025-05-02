294695650a83e5e42db9bb29777b047c1e7eac46
Brianna Marshall <brianna@bmarsha.com>
2025-05-02 10:23:16 -0400

    Use Z instead of i32 for repr relations

--- theories/iris/gc.v
+++ theories/iris/gc.v
@@ -25,27 +25,24 @@ Notation addr := N (only parsing).
 
 Notation addr_map := (gmap vloc addr).
 
-Definition code_int (z : Z) : i32 :=
-  Wasm_int.int_of_Z i32m (2 * z + 1).
+Definition code_int (i : Z) : Z := 2 * i + 1.
 
-Definition code_addr (a : addr) : i32 :=
-  Wasm_int.int_of_Z i32m (Z.of_N a).
+Definition code_addr (a : addr) : Z := Z.of_N a.
 
-Definition serialize_i32s (l : list i32) : bytes :=
-  flat_map serialise_i32 l.
+Definition serialize_z (i : Z) : bytes :=
+  serialise_i32 (Wasm_int.int_of_Z i32m i).
 
-Definition serialize_nat_32 (n : nat) : bytes :=
-  serialise_i32 (Wasm_int.int_of_Z i32m (Z.of_nat n)).
+Definition serialize_zs (l : list Z) : bytes :=
+  flat_map serialize_z l.
 
-Inductive repr_vval : addr_map -> vval -> i32 -> Prop :=
+Inductive repr_vval : addr_map -> vval -> Z -> Prop :=
   | RVint θ z :
       repr_vval θ (Vint z) (code_int z)
   | RVloc θ ℓ a :
       θ !! ℓ = Some a ->
-      (Z.of_N a < Wasm_int.Int32.modulus)%Z ->
       repr_vval θ (Vloc ℓ) (code_addr a).
 
-Inductive repr_vblock : addr_map -> vblock -> list i32 -> Prop :=
+Inductive repr_vblock : addr_map -> vblock -> list Z -> Prop :=
   | RVblock θ blk ks :
       length ks = length blk.(vals) ->
       Forall (curry (repr_vval θ)) (combine blk.(vals) ks) ->
@@ -53,11 +50,10 @@ Inductive repr_vblock : addr_map -> vblock -> list i32 -> Prop :=
 
 Definition vblock_offset (i : nat) : N := N.of_nat (4 * i).
 
-Inductive repr_vloc_offset : addr_map -> vloc -> nat -> i32 -> Prop :=
+Inductive repr_vloc_offset : addr_map -> vloc -> nat -> Z -> Prop :=
   | RVloc_offset θ ℓ i a a' :
       θ !! ℓ = Some a ->
       a' = (a + vblock_offset i)%N ->
-      (Z.of_N a' < Wasm_int.Int32.modulus)%Z ->
       repr_vloc_offset θ ℓ i (code_addr a').
 
 Definition roots_are_live (θ : addr_map) (roots : gmap addr vloc) : Prop :=
@@ -100,8 +96,8 @@ Definition GC (m : memaddr) (θ : addr_map) : iProp Σ :=
   ∃ (ζ : vstore) (roots : gmap addr vloc),
   ghost_map_auth gcG_vstore 1 ζ ∗
   ghost_map_auth gcG_roots 1 roots ∗
-  ([∗ map] ℓ ↦ a; blk ∈ θ; ζ, ∃ bs ks, N.of_nat m ↦[wms][a] bs ∗ ⌜bs = serialize_i32s ks⌝ ∗ ⌜repr_vblock θ blk ks⌝) ∗
-  ([∗ map] a ↦ ℓ ∈ roots, ∃ bs k, N.of_nat m ↦[wms][a] bs ∗ ⌜bs = serialise_i32 k⌝ ∗ ⌜repr_vloc_offset θ ℓ 0 k⌝) ∗
+  ([∗ map] ℓ ↦ a; blk ∈ θ; ζ, ∃ bs zs, N.of_nat m ↦[wms][a] bs ∗ ⌜bs = serialize_zs zs⌝ ∗ ⌜repr_vblock θ blk zs⌝) ∗
+  ([∗ map] a ↦ ℓ ∈ roots, ∃ bs z, N.of_nat m ↦[wms][a] bs ∗ ⌜bs = serialize_z z⌝ ∗ ⌜repr_vloc_offset θ ℓ 0 z⌝) ∗
   ⌜roots_are_live θ roots⌝ ∗
   ⌜GC_correct ζ θ⌝.
 
@@ -109,8 +105,8 @@ End GCtoken.
 
 Section GCrules.
 
-Context `{wasmG Σ}.
-Context `{rwasm_gcG Σ}.
+Context `{wasm : wasmG Σ}.
+Context `{rwasm : rwasm_gcG Σ}.
 
 Lemma wms_app n bs1 :
   forall ℓ sz1 bs2,
@@ -136,11 +132,11 @@ Proof.
   intros θ blk u ks i Hi Hblk. inversion Hblk. subst.
   assert (exists w, In (u, w) (combine (vals blk) ks)).
   - admit.
-  - destruct H3 as [w Hw].
+  - destruct H1 as [w Hw].
     exists w. split.
     + change (repr_vval θ u w) with (curry (repr_vval θ) (u, w)).
       eapply List.Forall_forall.
-      * exact H2.
+      * exact H0.
       * assumption.
     + admit.
 Admitted.
@@ -161,17 +157,21 @@ Proof.
   - unfold bits. reflexivity.
 Qed.
 
-Lemma vblock_offset_linear : forall n m,
-  vblock_offset (n + m) = (vblock_offset n + vblock_offset m)%N.
+Lemma pointer_offset_eqn_Z2N : forall i n m,
+  Z.of_N n = (Wasm_int.Int32.unsigned i + Z.of_N m)%Z ->
+  n = (Wasm_int.N_of_uint i32m i + m)%N.
 Proof.
-  intros n m. unfold vblock_offset. lia.
+  intros i n m.
+  rewrite <- (Z2N.id (Wasm_int.Int32.unsigned _)); last apply Wasm_int.Int32.unsigned_range.
+  rewrite <- N2Z.inj_add. apply N2Z.inj.
 Qed.
 
 Ltac solve_i32_bytes_len len :=
+  try rewrite <- flat_map_app;
   rewrite -> flat_map_constant_length with (c := 4);
-  [ rewrite len; unfold vblock_offset; unfold serialise_i32;
+  [ try rewrite length_app; rewrite len; unfold vblock_offset; unfold serialise_i32;
     try rewrite Memdata.encode_int_length;
-    lia
+    cbn; lia
   | auto ].
 
 Definition spec_alloc_gc
@@ -182,8 +182,8 @@ Definition spec_alloc_gc
   GC m θ ∗ N.of_nat fid ↦[wf] FC_func_native finst (Tf [T_i32] [T_i32]) fts fes ∗ ↪[frame] F -∗
   WP [AI_basic (BI_const (VAL_int32 n)); AI_invoke fid]
      @ E
-     {{ w, ∃ θ' ℓ i blk,
-        ⌜w = immV [VAL_int32 i]⌝ ∗ ⌜repr_vloc_offset θ' ℓ 0 i⌝ ∗
+     {{ w, ∃ θ' ℓ z blk,
+        ⌜w = immV [VAL_int32 (Wasm_int.int_of_Z i32m z)]⌝ ∗ ⌜repr_vloc_offset θ' ℓ 0 z⌝ ∗
         ⌜length blk.(vals) = Wasm_int.nat_of_uint i32m n⌝ ∗
         GC m θ' ∗ ℓ ↦vblk blk ∗
         N.of_nat fid ↦[wf] FC_func_native finst (Tf [T_i32] [T_i32]) fts fes ∗ ↪[frame] F }}%I.
@@ -191,20 +191,20 @@ Definition spec_alloc_gc
 Lemma wp_load_gc
     (s : stuckness) (E : coPset) (F : frame) (m : memaddr) (θ : addr_map)
     (i : i32) (ℓ : vloc) (blk : vblock)
-    (off1 off2 : nat) (off2b : static_offset) (vv : vval) :
+    (j : nat) (off : static_offset) (vv : vval) :
   F.(f_inst).(inst_memory) !! 0 = Some m ->
-  repr_vloc_offset θ ℓ off1 i ->
-  blk.(vals) !! (off1 + off2) = Some vv ->
-  vblock_offset off2 = off2b ->
+  repr_vloc_offset θ ℓ j (Wasm_int.Z_of_uint i32m i + Z.of_N off) ->
+  blk.(vals) !! j = Some vv ->
   GC m θ ∗ ℓ ↦vblk blk ∗ ↪[frame] F ⊢
-  WP [AI_basic (BI_const (VAL_int32 i)); AI_basic (BI_load T_i32 None N.zero off2b)]
+  WP [AI_basic (BI_const (VAL_int32 i)); AI_basic (BI_load T_i32 None N.zero off)]
      @ s; E
-     {{ w, (∃ k, ⌜w = immV [VAL_int32 k]⌝ ∗ ⌜repr_vval θ vv k⌝) ∗
+     {{ w, (∃ k, ⌜w = immV [VAL_int32 (Wasm_int.int_of_Z i32m k)]⌝ ∗ ⌜repr_vval θ vv k⌝) ∗
            GC m θ ∗ ℓ ↦vblk blk ∗ ↪[frame] F }}.
 Proof.
-  iIntros (Hmem Hrepr_loc Hvv Hoff)
+  iIntros (Hmem Hrepr_loc Hvv)
     "((%ζ & %roots & HGvstore & HGroots & Hvmem & Hrmem & %Hroots & %HGCC) & Hℓ & HF)".
-  inversion Hrepr_loc as [θ' ℓ' off1' a a' Hθℓ Ha' Hi32 Hθ' Hℓ' Hoff1' Hi]. subst.
+  inversion Hrepr_loc as [θ' ℓ' off' a a' Hθℓ Ha' Hθ' Hℓ' Hoff' Hi]. subst.
+  apply pointer_offset_eqn_Z2N in Hi.
   iDestruct (ghost_map_lookup with "HGvstore Hℓ") as "%Hζℓ".
   iDestruct (big_sepM2_lookup_acc _ _ _ _ _ _ Hθℓ Hζℓ with "Hvmem") as
     "[(%bs & %ks & Ha & %Hbs & %Hrepr_blk) Hvmem]".
@@ -213,37 +213,30 @@ Proof.
 
   (* Pull out the physical value in the vblock repr corresponding to the offset. *)
   destruct (list_pluck _ _ _ Hoff) as [ks1 [ki [ks2 [Hki [Hlen_ks1 Hks]]]]]. rewrite Hks.
-  destruct (repr_vblock_index θ blk vv ks (off1 + off2) Hvv Hrepr_blk) as [k [Hrepr_vv' Hk']].
+  destruct (repr_vblock_index θ blk vv ks j Hvv Hrepr_blk) as [k [Hrepr_vv' Hk']].
   assert (k = ki) by congruence. subst ki.
 
   (* Pull out the bytes in the vblock points-to corresponding to the offset. *)
-  unfold serialize_i32s. rewrite flat_map_app. rewrite (separate1 k). rewrite flat_map_app.
+  unfold serialize_zs. rewrite flat_map_app. rewrite (separate1 k). rewrite flat_map_app.
   simpl. rewrite app_nil_r.
-  rewrite (wms_app _ _ _ (vblock_offset (off1 + off2))).
-  rewrite (wms_app _ (serialise_i32 k) _ 4).
+  rewrite (wms_app _ _ _ (vblock_offset j)). rewrite Hi.
+  rewrite (wms_app _ (serialize_z k) _ 4).
   iDestruct "Ha" as "(Ha & Ha_off & Ha_rest)".
 
-  set ptr := (Wasm_int.N_of_uint i32m (code_addr (a + vblock_offset off1)) + vblock_offset off2)%N.
+  set ptr := (Wasm_int.N_of_uint i32m i + off)%N.
   set post := (λ w,
-    ((∃ k', ⌜w = immV [VAL_int32 k']⌝ ∗ ⌜repr_vval θ vv k'⌝) ∗
-     N.of_nat m ↦[wms][ptr] serialise_i32 k) ∗ ↪[frame] F
+    ((∃ k', ⌜w = immV [VAL_int32 (Wasm_int.int_of_Z i32m k')]⌝ ∗ ⌜repr_vval θ vv k'⌝) ∗
+     N.of_nat m ↦[wms][ptr] serialize_z k) ∗ ↪[frame] F
   )%I.
   iApply (wp_wand _ _ _ post with "[HF Ha_off] [HGvstore HGroots Hrmem Hℓ Ha Ha_rest Hvmem]").
   - (* Load the value from memory. *)
-    iApply wp_load_deserialize; auto.
-    unfold code_addr. rewrite deserialise_serialise_i32. cbn.
-    rewrite Wasm_int.Int32.Z_mod_modulus_id; last lia. rewrite N2Z.id.
-    rewrite vblock_offset_linear. rewrite N.add_assoc.
-    iFrame. by iExists k.
+    iApply wp_load_deserialize; auto. rewrite deserialise_serialise_i32. iFrame. by iExists k.
   - (* Show that the intermediate postcondition implies the original postcondition. *)
     iIntros (v) "[[HΦ Ha_off] HF]". iFrame "∗ %". iCombine "Ha Ha_off" as "Ha".
-    unfold code_addr. unfold ptr. cbn.
-    rewrite Wasm_int.Int32.Z_mod_modulus_id; last lia. rewrite N2Z.id.
-    rewrite <- (N.add_assoc _ (vblock_offset off1)). unfold vblock_offset.
-    rewrite <- wms_app; last (solve_i32_bytes_len Hlen_ks1).
+    unfold ptr. rewrite <- Hi. rewrite <- wms_app; last (solve_i32_bytes_len Hlen_ks1).
+    replace (serialize_z k) with (flat_map serialize_z [k]) by (by rewrite flat_map_singleton).
     iCombine "Ha Ha_rest" as "Ha". rewrite <- N.add_assoc.
-    rewrite <- wms_app; last (rewrite length_app; solve_i32_bytes_len (Hlen_ks1)).
-    replace (serialise_i32 k) with (flat_map serialise_i32 [k]) by (by rewrite flat_map_singleton).
+    rewrite <- wms_app; last (solve_i32_bytes_len Hlen_ks1).
     rewrite <- !flat_map_app. rewrite <- app_assoc. rewrite <- separate1. rewrite <- Hks.
     iApply "Hvmem". iExists _, _. by iFrame.
   - auto.
@@ -253,17 +246,16 @@ Qed.
 Lemma wp_store_gc
     (s : stuckness) (E : coPset) (F : frame) (m : memaddr) (θ : addr_map)
     (i k : i32) (ℓ : vloc) (blk blk' : vblock)
-    (off1 off2 : nat) (off2b : static_offset) (vv : vval) :
+    (j : nat) (off : static_offset) (vv : vval) :
   F.(f_inst).(inst_memory) !! 0 = Some m ->
-  repr_vloc_offset θ ℓ off1 i ->
-  vblock_offset off2 = off2b ->
-  off1 + off2 < length blk.(vals) ->
-  repr_vval θ vv k ->
-  blk' = Build_vblock blk.(tag) (<[ off1 + off2 := vv ]> blk.(vals)) ->
+  repr_vloc_offset θ ℓ j (Wasm_int.Z_of_uint i32m i + Z.of_N off) ->
+  j < length blk.(vals) ->
+  repr_vval θ vv (Wasm_int.Z_of_uint i32m k) ->
+  blk' = Build_vblock blk.(tag) (<[ j := vv ]> blk.(vals)) ->
   GC m θ ∗ ℓ ↦vblk blk ∗ ↪[frame] F ⊢
   WP [AI_basic (BI_const (VAL_int32 i));
       AI_basic (BI_const (VAL_int32 k));
-      AI_basic (BI_store T_i32 None N.zero off2b)]
+      AI_basic (BI_store T_i32 None N.zero off)]
      @ s; E
      {{ w, ⌜w = immV []⌝ ∗ GC m θ ∗ ℓ ↦vblk blk' ∗ ↪[frame] F }}.
 Admitted.
@@ -273,7 +265,7 @@ Definition spec_registerroot_gc
     (finst : instance) (fid : nat) (fts : list value_type) (fes : list basic_instruction)
     : iProp Σ :=
   □ ∀ (F : frame) (m : memaddr) (θ : addr_map) (ℓ : vloc) (i : i32),
-  GC m θ ∗ ⌜repr_vloc_offset θ ℓ 0 i⌝ ∗
+  GC m θ ∗ ⌜repr_vloc_offset θ ℓ 0 (Wasm_int.Z_of_uint i32m i)⌝ ∗
   N.of_nat fid ↦[wf] FC_func_native finst (Tf [T_i32] [T_i32]) fts fes ∗ ↪[frame] F -∗
   WP [AI_basic (BI_const (VAL_int32 i)); AI_invoke fid]
      @ E
@@ -289,7 +281,8 @@ Definition spec_unregisterroot_gc
   N.of_nat fid ↦[wf] FC_func_native finst (Tf [T_i32] [T_i32]) fts fes ∗ ↪[frame] F -∗
   WP [AI_basic (BI_const (VAL_int32 k)); AI_invoke fid]
      @ E
-     {{ w, ∃ i, ⌜w = immV [VAL_int32 i]⌝ ∗ ⌜repr_vloc_offset θ ℓ 0 i⌝ ∗ GC m θ ∗
+     {{ w, ∃ n, ⌜w = immV [VAL_int32 (Wasm_int.int_of_Z i32m n)]⌝ ∗ ⌜repr_vloc_offset θ ℓ 0 n⌝ ∗
+                GC m θ ∗
                 N.of_nat fid ↦[wf] FC_func_native finst (Tf [T_i32] [T_i32]) fts fes ∗ ↪[frame] F }}%I.
 
 End GCrules.
