b03a5f7ccd30105dcf0487a88ca7a4e7befec3ef
IRONM00N <me@ironmoon.dev>
2025-06-15 19:45:29 -0400

    locals monad example & incomplete proof

--- theories/compile.v
+++ theories/compile.v
@@ -251,6 +251,16 @@ Definition size_ctx :=
 
 Section TempLocals.
   Context `{!EqDecision wasm.value_type}.
+
+  #[global]
+  Instance wasm_value_type_eq_dec : EqDecision wasm.value_type.
+  Proof.
+    unfold EqDecision.
+    intros x y.
+    destruct x; destruct y;
+      (left; reflexivity) || (right; discriminate).
+  Defined.
+
   Record TempLocals := {
     tl_start : nat;
     tl_next  : nat;
@@ -297,6 +307,70 @@ Section TempLocals.
                         tl_free  := tl_free  st ∪ {[ i ]} |}).
 End TempLocals.
 
+Print exn.
+Section Example.
+  Context `{!EqDecision wasm.value_type}.
+
+  Let st0 : TempLocals :=
+    {| tl_start := 5;
+       tl_next := 5;
+       tl_types := ∅;
+       tl_free := ∅ |}.
+
+  Let tmp1 := match fresh_local wasm.T_i32 st0 with | OK (st1, i1) => (st1, i1) | _ => (st0, 0) end.
+  Let st1 := fst tmp1.
+  Let i1 := snd tmp1.
+
+  Let tmp2 := match fresh_local wasm.T_f64 st1 with | OK (st2, i2) => (st2, i2) | _ => (st1, 0) end.
+  Let st2 := fst tmp2.
+  Let i2 := snd tmp2.
+
+  Let tmp3 := match free_local i1 st2 with | OK (s,_) => s | _ => st2 end.
+  Let st3 := tmp3.
+
+  Let tmp4 := match fresh_local wasm.T_i32 st3 with | OK (st4, i3) => (st4, i3) | _ => (st3, 0) end.
+  Let st4 := fst tmp4.
+  Let i3 := snd tmp4.
+
+  Goal True.
+    assert (tl_types st4 !! i1 = Some wasm.T_i32) by (cbn; reflexivity).
+    assert (tl_types st4 !! i2 = Some wasm.T_f64) by (cbn; reflexivity).
+    assert (tl_types st4 !! i3 = Some wasm.T_i32)by (cbn; reflexivity).
+    assert (i3 = i1) by reflexivity.
+
+    exact I.
+  Qed.
+
+End Example.
+
+Lemma stateT_bind_OK_inv
+      {S A B}
+      (m : stateT S (exn Err) A)
+      (f : A → stateT S (exn Err) B)
+      (s : S) (st' : S) (b : B) :
+  (m ≫= f) s = @OK Err (S * B) (st', b) →
+  ∃ st1 a, m s = OK Err (S * A) (st1, a) ∧ f a st1 = OK Err (S * B) (st', b).
+Proof.
+  unfold mbind, stateT_MBind; simpl.
+  destruct (m s) as [e | [st1 a]] eqn:Hm; simpl.
+  - intros H. discriminate H.
+  - intros H. exists st1, a. split; [rewrite <- Hm | exact H]. reflexivity.
+Qed.
+
+Lemma fresh_local_preserves_tl_types ty st st' i :
+  fresh_local ty st = mret (st', i) →
+  tl_types st' !! i = Some ty
+  /\
+  (∀ j t, tl_types st !! j = Some t → tl_types st' !! j = Some t).
+Proof.
+  unfold fresh_local, get_st; cbn.
+  intro Hrun.
+    remember (filter (λ j, tl_types st !! j = Some ty)
+                   (elements (tl_free st))) as reusable eqn:Heq.
+  destruct reusable as [k |] eqn:Hr; cbn in Hrun.
+    - admit.
+    - admit.
+Admitted.
 
 Section compile_instr.
 
