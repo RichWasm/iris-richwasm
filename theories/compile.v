0e5c652c48e89807f662e131f85dad6fb89bd59c
Brianna Marshall <brianna@bmarsha.com>
2025-07-15 10:44:27 -0400

    Refactor ty_map_refs

--- theories/compile.v
+++ theories/compile.v
@@ -469,44 +469,33 @@ Definition layout_ty_length (layout : LayoutMode) (ty : R.Typ) :=
   | LMNative => length $ compile_typ ty
   end.
 
-Fixpoint ty_map_refs
-    (scope : VarScope)
-    (layout : LayoutMode)
-    (funcidx : W.immediate)
-    (ty : R.Typ)
-    (i : W.immediate) :
-    list W.basic_instruction :=
-  match ty with
-  | R.Num _
-  | R.Unit
-  | R.CoderefT _
-  | R.PtrT _
-  | R.OwnR _
-  | R.CapT _ _ _ => []
-  | R.ProdT tys =>
-      snd $ foldl
-        (fun '(j, es) ty' => (j + layout_ty_length layout ty',
-                              es ++ ty_map_refs scope layout funcidx ty' j))
-        (i, [])
-        tys
-  | R.Rec _ ty'
-  | R.ExLoc _ ty' => ty_map_refs scope layout funcidx ty' i
-  | R.TVar _
-  | R.RefT _ _ _ =>
-      let '(get, set) := scope_get_set scope in
-      get i ::
-      if_gc_bit_set [W.T_i32] [W.T_i32] [W.BI_call funcidx] [] ++
-      [set i]
-  end.
+Fixpoint ref_indices (layout : LayoutMode) (ty : R.Typ) : list W.immediate :=
+  let fix go ty i :=
+    match ty with
+    | R.Num _
+    | R.Unit
+    | R.CoderefT _
+    | R.PtrT _
+    | R.OwnR _
+    | R.CapT _ _ _ => []
+    | R.ProdT tys =>
+        snd $ foldl
+          (fun '(j, is) ty' => (j + layout_ty_length layout ty', is ++ go ty' j))
+          (i, [])
+          tys
+    | R.Rec _ ty'
+    | R.ExLoc _ ty' => go ty' i
+    | R.TVar _
+    | R.RefT _ _ _ => [i]
+    end in
+  go ty 0.
 
-Definition tys_map_refs
-    (scope : VarScope)
-    (layout : LayoutMode)
-    (funcidx : W.immediate)
-    (tys : list R.Typ)
-    (i : W.immediate) :
-    list W.basic_instruction :=
-  ty_map_refs scope layout funcidx (R.ProdT tys) i.
+Fixpoint map_gc_ref_vars (scope : VarScope) (func : W.immediate) : list W.immediate -> list W.basic_instruction :=
+  flat_map (fun i =>
+    let '(get, set) := scope_get_set scope in
+    get i ::
+    if_gc_bit_set [W.T_i32] [W.T_i32] [W.BI_call func] [] ++
+    [set i]).
 
 Section Fun.
 
@@ -544,7 +533,8 @@ Fixpoint compile_instr (instr: R.instr TyAnn) : wst (list W.basic_instruction) :
       | [t_drop] =>
           let wasm_typ := compile_typ t_drop in
           '(base, es) ← save_stack [t_drop];
-          mret $ ty_map_refs VSLocal LMNative funcidx_unregisterroot t_drop base ++
+          let ref_vars := map (Nat.add base) $ ref_indices LMNative t_drop in
+          mret $ map_gc_ref_vars VSLocal funcidx_unregisterroot ref_vars ++
                  restore_stack [t_drop] base ++
                  map (const W.BI_drop) wasm_typ
       | _ => mthrow (Err "drop should consume exactly one value")
@@ -571,33 +561,38 @@ Fixpoint compile_instr (instr: R.instr TyAnn) : wst (list W.basic_instruction) :
       let wdropped := flat_map compile_typ rdropped in
       '(idx_ret, ret_set_es) ← save_stack ret_ty';
       '(idx_dropped, dropped_es) ← save_stack rdropped;
+      let ref_vars := map (Nat.add idx_dropped) $ flat_map (ref_indices LMNative) rdropped in
       mret $ ret_set_es ++
              dropped_es ++
-             tys_map_refs VSLocal LMNative funcidx_unregisterroot rdropped idx_dropped ++
+             map_gc_ref_vars VSLocal funcidx_unregisterroot ref_vars ++
              restore_stack ret_ty' idx_ret ++
              [W.BI_return]
   | R.IGetLocal (R.Arrow targs trets, LSig L _) idx _ =>
       '(base, τ) ← liftM $ local_layout L 0 idx;
       let es1 := local_gets τ base in
       '(i, es2) ← save_stack [τ];
-      let es3 := ty_map_refs VSLocal LMNative funcidx_duproot τ i in
+      let ref_vars := map (Nat.add i) $ ref_indices LMNative τ in
+      let es3 := map_gc_ref_vars VSLocal funcidx_duproot ref_vars in
       let es4 := restore_stack [τ] i in
       mret $ es1 ++ es2 ++ es3 ++ es4
   | R.ISetLocal (R.Arrow targs trets, LSig L _) idx =>
       '(base, τ) ← liftM $ local_layout L 0 idx;
-      let es1 := ty_map_refs VSLocal LMWords funcidx_unregisterroot τ base in
+      let ref_vars := map (Nat.add base) $ ref_indices LMWords τ in
+      let es1 := map_gc_ref_vars VSLocal funcidx_unregisterroot ref_vars in
       es2 ← local_sets τ base;
       mret $ es1 ++ es2
   | R.IGetGlobal _ i =>
       '(i', ty) ← liftM $ guard_opt (Err "invalid global index") $ global_layout C.(global) i;
       let es1 := imap (fun j _ => W.BI_get_global $ i' + j) $ compile_typ ty in
       '(j, es2) ← save_stack [ty];
-      let es3 := ty_map_refs VSLocal LMNative funcidx_duproot ty j in
+      let ref_vars := map (Nat.add j) $ ref_indices LMNative ty in
+      let es3 := map_gc_ref_vars VSLocal funcidx_duproot ref_vars in
       let es4 := restore_stack [ty] i in
       mret $ es1 ++ es2 ++ es3 ++ es4
   | R.ISetGlobal _ i =>
       '(i', ty) ← liftM $ guard_opt (Err "invalid global index") $ global_layout C.(global) i;
-      let es1 := ty_map_refs VSGlobal LMNative funcidx_unregisterroot ty i' in
+      let ref_vars := map (Nat.add i') $ ref_indices LMNative ty in
+      let es1 := map_gc_ref_vars VSGlobal funcidx_unregisterroot ref_vars in
       let es2 := imap (fun j _ => W.BI_set_global (i' + j)) $ compile_typ ty in
       mret $ es1 ++ es2
   | R.ICoderef (R.Arrow targs trets, _) x => mthrow Todo
