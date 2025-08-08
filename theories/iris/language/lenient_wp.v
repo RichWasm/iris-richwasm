fbabafb6e43e905df16da0c9f0accefd205abf6b
Ryan Doenges <me@ryandoeng.es>
2025-08-08 13:57:46 -0400

    chore: clean up indentation and comments

--- theories/iris/language/lenient_wp.v
+++ theories/iris/language/lenient_wp.v
@@ -9,119 +9,55 @@ Section lenient_wp.
   Open Scope bi_scope.
 
   Section wp_params.
-  Variables (s: stuckness) (E: coPset).
+    Variables (s: stuckness) (E: coPset).
 
-  Definition lp_notrap (Φ: logpred) : val -> iProp Σ := 
-    λ lv,
-      match lv with
-      | trapV => False
-      | _ => lp_noframe Φ lv
-      end.
+    Definition lp_notrap (Φ: logpred) : val -> iProp Σ := 
+        λ lv,
+        match lv with
+        | trapV => False
+        | _ => lp_noframe Φ lv
+        end.
 
-  Definition lenient_wp (es: expr) (Φ: logpred): iProp Σ :=
-    wp s E es (denote_logpred Φ).
+    Definition lenient_wp (es: expr) (Φ: logpred): iProp Σ :=
+        wp s E es (denote_logpred Φ).
 
-  Definition lenient_wp_ctx (es: expr) (Φ: logpred) (i: nat) (lh: lholed) : iProp Σ :=
-    wp_wasm_ctx s E es (denote_logpred Φ) i lh.
+    Definition lenient_wp_ctx (es: expr) (Φ: logpred) (i: nat) (lh: lholed) : iProp Σ :=
+        wp_wasm_ctx s E es (denote_logpred Φ) i lh.
 
-  Lemma lp_trap_sep (Φ Ψ: logpred) :
-    lp_trap (Φ ∗ Ψ) ⊣⊢ lp_trap Φ ∗ lp_trap Ψ.
-  Proof.
-    reflexivity.
-  Qed.
+    Lemma lp_trap_sep (Φ Ψ: logpred) :
+        lp_trap (Φ ∗ Ψ) ⊣⊢ lp_trap Φ ∗ lp_trap Ψ.
+    Proof.
+        reflexivity.
+    Qed.
 
-  (* TODO move this to the structural rules file with wp_seq_can_trap_ctx and the rest of those lemmas *)
-  Lemma wp_seq_can_trap (Φ Ψ : iris.val -> iProp Σ) (es1 es2 : language.expr wasm_lang) (Φf : datatypes.frame -> iProp Σ) (Φt : iProp Σ) :
-    (Ψ trapV ={E}=∗ ⌜False⌝) ∗
-    (∀ f, ↪[frame] f ∗ Φt ∗ Φf f -∗ Φ trapV) ∗
-    WP es1 @ NotStuck; E {{ w, (⌜w = trapV⌝ ∗ Φt ∨ Ψ w) ∗ ∃ f0, ↪[frame] f0 ∗ Φf f0 }} ∗
-    (∀ w (f0: datatypes.frame),
-        Ψ w ∗ ↪[frame] f0 ∗ Φf f0 -∗
-        WP (of_val w ++ es2) @ s; E {{ v, Φ v }})
-    ⊢ WP (es1 ++ es2) @ s; E {{ v, Φ v }}.
-  Proof.
-    iIntros "(HΨnotrap & Hframetrap & Hes1 & Hes2)".
-    iApply wp_wasm_empty_ctx.
-    iApply wp_seq_can_trap_ctx_precise; iFrame.
-    iIntros (w f0) "(Hw & Hf0 & HΦf)".
-    Locate "CTX_EMPTY".
-    Search (LH_base nil nil) wp_wasm_ctx.
-    unfold wp_wasm_ctx.
-    iIntros (LI Hfill).
-    assert (LI = (iris.of_val w ++ es2)).
-    {
-      cbn in *.
-      rewrite app_nil_r in Hfill.
-      by destruct (@seq.eqseqP _ LI (iris.of_val w ++ es2)).
-    }
-    subst LI.
-    iApply "Hes2".
-    iFrame.
-  Qed.
-
-  (*
-  Definition has_a_frame (Φ: iProp Σ) :=
-    ∃ Φ' (f: datatypes.frame), □(Φ ∗-∗ Φ' ∗ ↪[frame] f).
-
-  Lemma wp_seq_can_trap' (Φ Ψ : iris.val -> iProp Σ) (Ψt: iProp Σ) (es1 es2 : language.expr wasm_lang) f :
-    ↪[frame] f ∗
-    (∀ w, has_a_frame (Ψ w)) ∗
-    (∀ w, has_a_frame (Φ w)) ∗
-    (((Ψ trapV ={E}=∗ ⌜False⌝)) ∗
-     (Ψt -∗ Φ trapV) ∗
-     (WP es1 @ NotStuck; E {{ w, (⌜w = trapV⌝ ∗ Ψt) ∨ Ψ w}}) ∗
-     ∀ w, Ψ w -∗ WP (iris.of_val w ++ es2) @ s; E {{ v, Φ v }})%I
-     ⊢ WP (es1 ++ es2) @ s; E {{ v, Φ v }}.
-  Proof.
-    iLöb as "IH" forall (Φ Ψ Ψt es1 es2 f).
-    iIntros "(Hf & HfΨ & HfΦ & Hnotrap & Httrap & Hes1 & Hes2)".
-    repeat rewrite wp_unfold /wp_pre /=.
-    destruct (iris.to_val es1) as [vs|] eqn:Hes1tov.
-    {
-      iMod ("Hes1").
-      iDestruct "Hes1" as "[[-> Hes1] | Hes1]".
-      {
-        apply to_val_trap_is_singleton in Hes1tov as ->.
-        destruct es2; cycle 1.
+    (* TODO move this to the structural rules file with wp_seq_can_trap_ctx and the rest of those lemmas *)
+    Lemma wp_seq_can_trap (Φ Ψ : iris.val -> iProp Σ) (es1 es2 : language.expr wasm_lang) (Φf : datatypes.frame -> iProp Σ) (Φt : iProp Σ) :
+        (Ψ trapV ={E}=∗ ⌜False⌝) ∗
+        (∀ f, ↪[frame] f ∗ Φt ∗ Φf f -∗ Φ trapV) ∗
+        WP es1 @ NotStuck; E {{ w, (⌜w = trapV⌝ ∗ Φt ∨ Ψ w) ∗ ∃ f0, ↪[frame] f0 ∗ Φf f0 }} ∗
+        (∀ w (f0: datatypes.frame),
+            Ψ w ∗ ↪[frame] f0 ∗ Φf f0 -∗
+            WP (of_val w ++ es2) @ s; E {{ v, Φ v }})
+        ⊢ WP (es1 ++ es2) @ s; E {{ v, Φ v }}.
+    Proof.
+        iIntros "(HΨnotrap & Hframetrap & Hes1 & Hes2)".
+        iApply wp_wasm_empty_ctx.
+        iApply wp_seq_can_trap_ctx_precise; iFrame.
+        iIntros (w f0) "(Hw & Hf0 & HΦf)".
+        Locate "CTX_EMPTY".
+        Search (LH_base nil nil) wp_wasm_ctx.
+        unfold wp_wasm_ctx.
+        iIntros (LI Hfill).
+        assert (LI = (iris.of_val w ++ es2)).
         {
-          iSpecialize ("Httrap" with "Hes1").
-          iAssert (has_a_frame (Φ trapV)) with "[HfΦ]" as "HfΦ'".
-          iApply "HfΦ".
-          iDestruct "HfΦ'" as (Φ' fr) "#Hsplit".
-          iAssert (Φ' ∗ ↪[frame] fr) with "[Httrap]" as "[HΦ' Hfr]".
-          by iApply "Hsplit".
-          iApply (wp_wand _ _ _ (λ v, Φ' ∗ ↪[frame] fr) with "[HΦ' Hfr]").
-          change [AI_trap] with ([] ++ [AI_trap]).
-          rewrite -app_assoc.
-          iApply (wp_trap with "[HΦ'] [$Hfr]").
-          done.
-          iFrame.
-          done.
-          iApply "Hsplit" in "Httrap".
-
-
-          iApply "Hes2".
-          assert (iris.to_val ([AI_trap] ++ a :: es2) = None) as Hnone
-              by (rewrite -(app_nil_l [AI_trap]) -app_assoc; apply to_val_not_trap_interweave; auto).
-          rewrite /has_a_frame.
-          iRewrite "HfΦ".
-          Search wp AI_trap.
-          eapply wp_trap.
-          rewrite Hnone.
-          congruence.
-
+        cbn in *.
+        rewrite app_nil_r in Hfill.
+        by destruct (@seq.eqseqP _ LI (iris.of_val w ++ es2)).
         }
-          eapply to_val_None_lfilled in Hnone; [|eauto].
-        
-      unfold iris
-      iDestruct H
-
-      eapply lfilled_to_val_app in Hes1tov as HH;eauto.
-      destruct HH as [vs' [Hvs' Hfilled']].
-      unfold iris_wp_def.to_val in Hvs'.
-    rewrite Hvs'.
-      apply to_val_trap_is_singleton in Hvs'.
-      *)
+        subst LI.
+        iApply "Hes2".
+        iFrame.
+    Qed.
 
   End wp_params.
 
