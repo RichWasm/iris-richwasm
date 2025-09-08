724ed35e86b6a99d5b7ed3a3c947b36ca82dbba1
IRONM00N <me@ironmoon.dev>
2025-09-08 15:30:26 -0400

    make miniml compile

--- src/mini-ml/syntax.ml
+++ src/mini-ml/syntax.ml
@@ -1,3 +1,5 @@
+open Base
+
 module Tag = struct
   type t = int [@@deriving sexp, equal]
 
@@ -5,7 +7,7 @@ module Tag = struct
     let x = ref 0 in
     fun () ->
       let y = !x in
-      x := x + 1;
+      x := !x + 1;
       y
   ;;
 end
@@ -19,7 +21,17 @@ module Source = struct
     type t = string [@@deriving sexp]
   end
 
-  module rec Type = struct
+  module rec Type : sig
+    type t =
+      | Int
+      | Var of TypeVar.t
+      | Fun of FunType.t
+      | Prod of t list
+      | Sum of t list
+      | Ref of t
+      | Rec of TypeVar.t * t
+      | LL of t (* TODO: should this be an LL type? *)
+  end = struct
     type t =
       | Int
       | Var of TypeVar.t
@@ -31,7 +43,12 @@ module Source = struct
       | LL of t (* TODO: should this be an LL type? *)
     [@@deriving sexp]
   end
-  and FunType = struct
+  and FunType : sig
+    type t =
+      { foralls : TypeVar.t list
+      ; args : Type.t list
+      ; ret : Type.t }
+  end = struct
     type t =
       { foralls : TypeVar.t list
       ; args : Type.t list
@@ -39,7 +56,17 @@ module Source = struct
     [@@deriving sexp]
   end
 
-  module rec Value = struct
+  module rec Value : sig
+    type t =
+      | Int of int
+      | Var of Variable.t
+      | Lambda of
+          { foralls : TypeVar.t list
+          ; args : (Variable.t * Type.t) list
+          ; body : Expr.t }
+      | Prod of t list
+      | Inj of int * t
+  end = struct
     type t =
       | Int of int (** FIXME: make this an int31 *)
       | Var of Variable.t
@@ -51,7 +78,20 @@ module Source = struct
       | Inj of int * t
     [@@deriving sexp]
   end
-  and Computation = struct
+  and Computation : sig
+    type t =
+      | Apply of Value.t * Type.t list * Value.t list
+      | Let of Variable.t * Expr.t * Expr.t
+      | If0 of Value.t * Expr.t * Expr.t
+      | Op of [ `Add | `Sub | `Mul | `Div ] * Value.t * Value.t
+      | Cases of Value.t * (int * Variable.t * Expr.t) list
+      | New of Value.t
+      | Deref of Value.t
+      | Assign of Value.t * Value.t
+      | Project of int * Value.t
+      | Fold of Type.t * Value.t
+      | Unfold of Value.t
+  end = struct
     type t =
       | Apply of Value.t * Type.t list * Value.t list
       | Let of Variable.t * Expr.t * Expr.t
@@ -66,134 +106,20 @@ module Source = struct
       | Unfold of Value.t
     [@@deriving sexp]
   end
-  and Expr = struct
+  and Expr : sig
+    type t =
+      | Value of Value.t
+      | Computation of Computation.t
+  end = struct
     type t =
       | Value of Value.t
       | Computation of Computation.t
     [@@deriving sexp]
   end
-end
-
-module Tagged = struct
-  module Variable = Source.Variable
-  module TypeVar = Source.TypeVar
-
-  module Type = struct
-    type t = Tag.t * Source.Type.t [@@deriving sexp]
-  end
-  module FunType = Source.FunType
-
-  module Value = Source.Value
-  module Computation = Source.Computation
-  module Expr = struct
-    type t = Tag.t * Source.Expr.t [@@deriving sexp]
-  end
-
-  module Module = struct
-    type t = Tag.t * Source.Module.t [@@deriving sexp]
+  and Module : sig
+    type t
+  end = struct
+    type t
   end
 end
 
-module Debruijned = struct
-  module Variable = struct
-    type t = int [@@deriving sexp]
-  end
-
-  module TypeVar = struct
-    type t = int [@@driving sexp]
-  end
-
-  module rec Type = struct
-    type t_base =
-      | Int
-      | Var of Variable.t
-      | Fun of FunType.t
-      | Prod of t list
-      | Sum of t list
-      | Ref of t
-      | Rec of TypeVar.t * t
-      | LL of t
-    and t = Tag.t * t_base [@@deriving sexp]
-  end
-  and FunType = struct
-    type t =
-      { foralls : TypeVar.t list
-      ; args : Type.t list
-      ; ret : Type.t }
-    [@@deriving sexp]
-  end
-
-  module Value = struct
-    type t =
-      | Int of int
-      | Var of Variable.t
-      | Lambda of
-          { foralls : TypeVar.t list
-          ; args : (Variable.t * Type.t) list
-          ; body : Expr.t }
-      | Prod of t list
-      | Inj of int * t
-    [@@deriving sexp]
-  end
-  and Computation = struct
-    type t =
-      | Apply of Value.t * Type.t list * Value.t list
-      | Let of Variable.t * Expr.t * Expr.t
-      | If0 of Value.t * Expr.t * Expr.t
-      | Op of [`Add | `Sub | `Mul | `Div ] * Value.t * Value.t
-      | Cases of Value.t * (int * Variable.t * Expr.t) list
-      | New of Value.t
-      | Deref of Value.t
-      | Assign of Value.t * Value.t
-      | Project of int * Value.t
-      | Fold of Type.t * Value.t
-      | Unfold of Value.t
-  end
-  and Expr = struct
-    type t_base =
-      | Value of Value.t
-      | Computation of Computation.t
-    and t = Tag.t * t_base [@@deriving sexp]
-
-  module Module = struct
-    type import =
-      { typ : FunType.t
-      ; mod_name : Variable.t
-      ; fun_name : Variable.t }
-    [@@deriving sexp]
-
-    type func =
-      { foralls : TypeVar.t list
-      ; args : (Variable.t * Type.t) list
-      ; body : Expr.t }
-    [@@deriving sexp]
-
-    type t =
-      | LetIm of import * t
-      | LetEx of Variable.t * func * t
-      | Global of Variable.t * Expr.t * t
-      | Body of Expr.t
-    [@@deriving sexp]
-  end
-
-  module Module = struct
-    type import =
-      { typ : FunType.t
-      ; mod_name : Variable.t
-      ; fun_name : Variable.t }
-    [@@deriving sexp]
-
-    type func =
-      { foralls : TypeVar.t list
-      ; args : (Variable.t * Type.t) list
-      ; body : Expr.t }
-    [@@deriving sexp]
-
-    type t =
-      | LetIm of import * t
-      | LetEx of Variable.t * func * t
-      | Global of Variable.t * Expr.t * t
-      | Body of Expr.t
-    [@@deriving sexp]
-  end
-end
