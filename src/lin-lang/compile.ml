81b435b5e7cad25552218cb8c0295b35c0ee89c6
IRONM00N <me@ironmoon.dev>
2025-09-14 16:15:12 -0400

    lin-lang: continue designing cc IR

---
+++ src/lin-lang/compile.ml
@@ -0,0 +1,9 @@
+open Syntax
+open Ir
+
+module ClosureConversion = struct
+
+let closure_conversion (modul : Syntax.Module) =
+  match modul with
+  | Module() -> 
+end
--- src/lin-lang/ir.ml
+++ src/lin-lang/ir.ml
@@ -2,7 +2,7 @@ open Sexplib.Std
 
 module Types = struct
   type variable = string
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   type typ =
     | Int
@@ -11,38 +11,40 @@ module Types = struct
     | Lolipop of typ * typ
     | Var of variable
     | Exists of variable * typ
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   type binding = variable * typ
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   type value =
     | Var of variable
+    | Coderef of variable
     | Int of int
     | Prod of value * value
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   and expr =
     | Val of value
     | App of value * value
     | Let of binding * expr * expr
     | If0 of value * expr * expr
-    | Binop of [ `Add | `Sub | `Mul | `Div ] * value * value
+    | Binop of [`Add | `Sub | `Mul | `Div] * value * value
     | LetPair of binding * binding * expr * expr
     | New of value
     | Swap of value * value
     | Free of value
-    | Pack of
-    | Unpack of
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+    | Pack of typ * value * value
+    | Unpack of variable * binding * value * expr
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   type import = Import of typ * variable
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
-  type toplevel = Let of bool * binding * expr (* export *)
-  | Func of bool * variable * binding list * typ * expr
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  type toplevel =
+    | Let of bool * binding * expr (* export *)
+    | Func of bool * variable * binding list * typ * expr
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 
   type modul = Module of import list * toplevel list * expr option
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  [@@deriving show {with_path= false}, eq, iter, map, fold, sexp]
 end
