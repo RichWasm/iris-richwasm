0df198c3fdc8fc124e649bad0e53d9952c934ab5
IRONM00N <me@ironmoon.dev>
2025-09-15 12:40:56 -0400

    lin-lang: update cc ir

--- src/lin-lang/cc.ml
+++ src/lin-lang/cc.ml
@@ -1,50 +1,60 @@
-module Types = struct
+module Declosure = struct
   open Sexplib.Std
-
-  type variable = string
-  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+  open Index
 
   type typ =
     | Int
     | Prod of typ * typ
     | Ref of typ
-    | Lolipop of typ * typ
-    | Var of variable
-    | Exists of variable * typ
+    | Lolipop of typ list * typ (* no closures *)
+    | Var of int (* type de Bruijn *)
+    | Exists of typ
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
-  type binding = variable * typ
+  type binop = Indexed.binop
+  [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+
+  type gvar = Indexed.gvar
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
   type value =
-    | Var of variable
-    | Coderef of variable
+    | Var of int (* val de Bruijn *)
+    | Global of gvar
+    | Coderef of gvar
     | Int of int
     | Prod of value * value
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
-  and expr =
+  type expr =
     | Val of value
     | App of value * value
-    | Let of binding * expr * expr
+    | Let of typ * expr * expr
     | If0 of value * expr * expr
-    | Binop of [ `Add | `Sub | `Mul | `Div ] * value * value
-    | LetPair of binding * binding * expr * expr
+    | Binop of binop * value * value
+    | LetPair of typ * typ * expr * expr
     | New of value
     | Swap of value * value
     | Free of value
-    | Pack of typ * value * value
-    | Unpack of variable * binding * value * expr
+    | Pack of typ * value * typ
+    | Unpack of value * expr * typ
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
-  type import = Import of typ * variable
+  type import = Import of typ * gvar
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
   type toplevel =
-    | Let of bool * binding * expr (* export *)
-    | Func of bool * variable * binding list * typ * expr
+    | Let of bool * (gvar * typ) * expr (* export *)
+    | Func of bool * gvar * typ list * typ * expr
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
 
   type modul = Module of import list * toplevel list * expr option
   [@@deriving show { with_path = false }, eq, iter, map, fold, sexp]
+end
+
+module CCErr = struct
+  type t
+end
+
+module ClosureConversion = struct
+
 end
--- src/lin-lang/compile.ml
+++ src/lin-lang/compile.ml
@@ -1,12 +1,3 @@
 open! Syntax
-open! Ir
-module AlphaRename = struct end
-module LiftExports = struct end
+open! Cc
 
-module ClosureConversion = struct
-  (*
-let closure_conversion (modul : Syntax.Module) =
-  match modul with
-  | Module (imports, toplevels, ) -> Ir.Types.Module () *)
-
-end
