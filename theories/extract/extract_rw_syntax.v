7ff775cc753fbf18554df81851959a080996a2da
IRONM00N <me@ironmoon.dev>
2025-09-21 23:34:28 -0400

    extraction: extract nat as bigint

--- theories/extract/extract_rw_syntax.v
+++ theories/extract/extract_rw_syntax.v
@@ -2,6 +2,10 @@ From Stdlib Require Import Extraction ExtrOcamlBasic ExtrOcamlZBigInt.
 
 Set Warnings "-extraction-default-directory".
 
+Extract Inductive nat => "Z.t"
+  [ "Z.zero" "Z.succ" ]
+  "(fun fO fS n -> if Z.equal n Z.zero then fO () else fS (Z.pred n))".
+
 Extraction Language OCaml.
 Set Extraction AutoInline.
 Set Extraction Optimize.
