d1d9ec216713d8567831e978c8e1402069a66707
IRONM00N <me@ironmoon.dev>
2025-09-26 15:51:28 -0400

    ocaml-common: wip richwasm pretty printer

--- src/common/RichWasm.ml
+++ src/common/RichWasm.ml
@@ -1,4 +1,5 @@
 open! Base
+open Stdlib.Format
 
 module Big_int_Z = struct
   include Z
@@ -16,90 +17,170 @@ module Path = struct
     type t =
       [%import:
         (Richwasm_extract.RwSyntax.path_component[@with Z.t := Big_int_Z.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
   end
 
   type t =
     [%import:
       (Richwasm_extract.RwSyntax.path[@with path_component := Component.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
 end
 
 module ConcreteMemory = struct
   type t = [%import: Richwasm_extract.RwSyntax.Core.concrete_memory]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
 end
 
 module Copyability = struct
   type t = [%import: Richwasm_extract.RwSyntax.Core.copyability]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
 end
 
 module Dropability = struct
   type t = [%import: Richwasm_extract.RwSyntax.Core.dropability]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
 end
 
 module PrimitiveRep = struct
   type t = [%import: Richwasm_extract.RwSyntax.Core.primitive_rep]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff x = Sexp.pp_hum ff (sexp_of_t x)
 end
 
 module Sign = struct
   type t = [%import: Richwasm_extract.RwSyntax.Core.sign]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff : t -> unit = function
+    | SignU -> fprintf ff "u"
+    | SignS -> fprintf ff "s"
 end
 
 module Float = struct
   module Type = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.float_type]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | F32T -> fprintf ff "f32"
+      | F64T -> fprintf ff "f64"
   end
 
   module Unop = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.float_unop]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | NegF -> fprintf ff "neg"
+      | AbsF -> fprintf ff "abs"
+      | CeilF -> fprintf ff "ceil"
+      | FloorF -> fprintf ff "floor"
+      | TruncF -> fprintf ff "trunc"
+      | NearestF -> fprintf ff "nearest"
+      | SqrtF -> fprintf ff "sqrt"
   end
 
   module Binop = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.float_binop]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | AddF -> fprintf ff "add"
+      | SubF -> fprintf ff "sub"
+      | MulF -> fprintf ff "mul"
+      | DivF -> fprintf ff "div"
+      | MinF -> fprintf ff "min"
+      | MaxF -> fprintf ff "max"
+      | CopySignF -> fprintf ff "copysign"
   end
 
   module Relop = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.float_relop]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | EqF -> fprintf ff "eq"
+      | NeF -> fprintf ff "ne"
+      | LtF -> fprintf ff "lt"
+      | GtF -> fprintf ff "gt"
+      | LeF -> fprintf ff "le"
+      | GeF -> fprintf ff "ge"
   end
 end
 
 module Int = struct
   module Type = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.int_type]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | I32T -> fprintf ff "i32"
+      | I64T -> fprintf ff "i64"
   end
 
   module Unop = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.int_unop]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | ClzI -> fprintf ff "clz"
+      | CtzI -> fprintf ff "ctz"
+      | PopcntI -> fprintf ff "popcnt"
   end
 
   module Binop = struct
     type t =
       [%import:
         (Richwasm_extract.RwSyntax.Core.int_binop[@with sign := Sign.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | AddI -> fprintf ff "add"
+      | SubI -> fprintf ff "sub"
+      | MulI -> fprintf ff "mul"
+      | DivI s -> fprintf ff "div_%a" Sign.pp s
+      | RemI s -> fprintf ff "rem_%a" Sign.pp s
+      | AndI -> fprintf ff "and"
+      | OrI -> fprintf ff "or"
+      | XorI -> fprintf ff "xor"
+      | ShlI -> fprintf ff "shl"
+      | ShrI s -> fprintf ff "shr_%a" Sign.pp s
+      | RotlI -> fprintf ff "rotl"
+      | RotrI -> fprintf ff "rotr"
   end
 
   module Relop = struct
     type t =
       [%import:
         (Richwasm_extract.RwSyntax.Core.int_relop[@with sign := Sign.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | EqI -> fprintf ff "eq"
+      | NeI -> fprintf ff "ne"
+      | LtI s -> fprintf ff "lt_%a" Sign.pp s
+      | GtI s -> fprintf ff "gt_%a" Sign.pp s
+      | LeI s -> fprintf ff "le_%a" Sign.pp s
+      | GeI s -> fprintf ff "ge_%a" Sign.pp s
   end
 
   module Testop = struct
     type t = [%import: Richwasm_extract.RwSyntax.Core.int_testop]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp]
+
+    let pp ff : t -> unit = function
+      | EqzI -> fprintf ff "eqz"
   end
 end
 
@@ -111,7 +192,22 @@ module ConversionOp = struct
         sign := Sign.t;
         int_type := Int.Type.t;
         float_type := Float.Type.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp_sexp ff x = Sexp.pp_hum ff (sexp_of_t x)
+
+  let pp ff : t -> unit = function
+    | CWrap -> fprintf ff "i32.wrap_i64"
+    | CExtend s -> fprintf ff "i64.extend_i32_%a" Sign.pp s
+    (* | CTrunc (int_type, float_type, sign) -> failwith "TODO"
+    | CTruncSat (int_type, float_type, sign) -> failwith "TODO" *)
+    | CDemote -> fprintf ff "f32.demote_f64"
+    | CPromote -> fprintf ff "f64.promote_f32"
+    (* | CConvert (float_type, int_type, sign) -> failwith "TODO, ordering"
+    | CReinterpretFI (float_type, int_type) -> failwith "TODO"
+    | CReinterpretIF (int_type, float_type) -> failwith "TODO"
+    | CReinterpretII (int_type, sign1, sign2) -> failwith "TODO" *)
+    | x -> pp_sexp ff x
 end
 
 module NumInstruction = struct
@@ -129,7 +225,17 @@ module NumInstruction = struct
         float_binop := Float.Binop.t;
         float_relop := Float.Relop.t;
         conversion_op := ConversionOp.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+  let pp ff : t -> unit = function
+    | IInt1 (t, o) -> fprintf ff "%a.%a" Int.Type.pp t Int.Unop.pp o
+    | IInt2 (t, o) -> fprintf ff "%a.%a" Int.Type.pp t Int.Binop.pp o
+    | IIntTest (t, o) -> fprintf ff "%a.%a" Int.Type.pp t Int.Testop.pp o
+    | IIntRel (t, o) -> fprintf ff "%a.%a" Int.Type.pp t Int.Relop.pp o
+    | IFloat1 (t, o) -> fprintf ff "%a.%a" Float.Type.pp t Float.Unop.pp o
+    | IFloat2 (t, o) -> fprintf ff "%a.%a" Float.Type.pp t Float.Binop.pp o
+    | IFloatRel (t, o) -> fprintf ff "%a.%a" Float.Type.pp t Float.Relop.pp o
+    | ICvt o -> fprintf ff "%a" ConversionOp.pp o
 end
 
 module Representation = struct
@@ -139,7 +245,7 @@ module Representation = struct
       [@with
         Z.t := Big_int_Z.t;
         primitive_rep := PrimitiveRep.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module Size = struct
@@ -149,13 +255,13 @@ module Size = struct
       [@with
         Z.t := Big_int_Z.t;
         representation := Representation.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module Sizity = struct
   type t =
     [%import: (Richwasm_extract.RwSyntax.Core.sizity[@with size := Size.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module Memory = struct
@@ -165,7 +271,7 @@ module Memory = struct
       [@with
         Z.t := Big_int_Z.t;
         concrete_memory := ConcreteMemory.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module Kind = struct
@@ -178,7 +284,7 @@ module Kind = struct
         dropability := Dropability.t;
         sizity := Sizity.t;
         memory := Memory.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module NumType = struct
@@ -188,7 +294,11 @@ module NumType = struct
       [@with
         int_type := Int.Type.t;
         float_type := Float.Type.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+  let pp ff : t -> unit = function
+    | IntT it -> Int.Type.pp ff it
+    | FloatT ft -> Float.Type.pp ff ft
 end
 
 (* work around bug? with ppx_import *)
@@ -206,6 +316,7 @@ module Internal = struct
           size := Size.t;
           coq_type := typ;
           function_type := function_typ])]
+    [@@deriving show { with_path = false }]
 
     and instruction_typ =
       [%import:
@@ -213,6 +324,7 @@ module Internal = struct
         [@with
           coq_type := typ;
           function_type := function_typ])]
+    [@@deriving show { with_path = false }]
 
     and function_typ =
       [%import:
@@ -221,22 +333,43 @@ module Internal = struct
           kind := Kind.t;
           instruction_type := instruction_typ;
           function_type := function_typ])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
   end
 end
 
 module Type = struct
-  type t = Internal.Types.typ [@@deriving show, eq, ord, iter, map, fold, sexp]
+  include Internal.Types
+
+  type t = Internal.Types.typ
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module InstructionType = struct
+  include Internal.Types
+
   type t = Internal.Types.instruction_typ
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let pp ff (InstrT (from, res) : t) : unit =
+    fprintf ff "(@[";
+    List.iter ~f:(fprintf ff "%a@ " Type.pp) from;
+    fprintf ff "->";
+    List.iter ~f:(fprintf ff "@ %a" Type.pp) res;
+    fprintf ff "@])"
 end
 
 module FunctionType = struct
+  include Internal.Types
+
   type t = Internal.Types.function_typ
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp]
+
+  let rec pp ff : t -> unit = function
+    | MonoFunT it -> InstructionType.pp ff it
+    | ForallMemT ft -> fprintf ff "(forall.mem %a)" pp ft
+    | ForallRepT ft -> fprintf ff "(forall.rep %a)" pp ft
+    | ForallSizeT ft -> fprintf ff "(forall.size %a)" pp ft
+    | ForallTypeT (k, ft) -> fprintf ff "(forall.type %a %a)" Kind.pp k pp ft
 end
 
 module Index = struct
@@ -250,7 +383,7 @@ module Index = struct
         coq_type := Type.t;
         instruction_type := InstructionType.t;
         function_type := FunctionType.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
 end
 
 module LocalFx = struct
@@ -260,7 +393,13 @@ module LocalFx = struct
       [@with
         Z.t := Big_int_Z.t;
         coq_type := Type.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+  let pp_ ff (lfx : t) : unit =
+    failwith "TODO";
+    fprintf ff "";
+    List.iter ~f:(fun (i, t) -> fprintf ff "") lfx;
+    fprintf ff ""
 end
 
 module Instruction = struct
@@ -274,13 +413,37 @@ module Instruction = struct
         local_fx := LocalFx.t;
         index := Index.t;
         path := Path.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+  let show_pp = pp
+
+  let rec pp ff : t -> unit =
+    let pp_instrs ff (instrs : t list) = () in
+    function
+    | INop _ -> fprintf ff "nop"
+    | IUnreachable _ -> fprintf ff "unreachable"
+    | ICopy _ -> fprintf ff "copy"
+    | IDrop _ -> fprintf ff "drop"
+    | INum (_, ni) -> fprintf ff "%a" NumInstruction.pp ni
+    | INumConst (InstrT ([], [ NumT (_, t) ]), n) ->
+        fprintf ff "%a.const %a" NumType.pp t Big_int_Z.pp n
+        (*| IBlock (it, lfx, instrs) ->
+        (* TODO: block return *)
+        fprintf ff "block %a %a @; @[<v 2>%a@] end" InstructionType.pp it
+          LocalFx.pp lfx pp_instrs instrs *)
+    | IReturn _ -> fprintf ff "return"
+    | ILocalGet (_, i) -> fprintf ff "local.get %a" Big_int_Z.pp i
+    | ILocalSet (_, i) -> fprintf ff "local.set %a" Big_int_Z.pp i
+    | IGlobalGet (_, i) -> fprintf ff "global.get %a" Big_int_Z.pp i
+    | IGlobalSet (_, i) -> fprintf ff "global.set %a" Big_int_Z.pp i
+    | IGlobalSwap (_, i) -> fprintf ff "global.swap %a" Big_int_Z.pp i
+    | x -> show_pp ff x
 end
 
 module Module = struct
   module Mutability = struct
     type t = [%import: Richwasm_extract.RwSyntax.mutability]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
   end
 
   module Function = struct
@@ -291,7 +454,13 @@ module Module = struct
           Core.function_type := FunctionType.t;
           Core.representation := Representation.t;
           Core.instruction := Instruction.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+    let pp ff ({ mf_type; mf_body } : t) : unit =
+      (* FIXME: where are locals?? *)
+      fprintf ff "@[<v 2>@[(func %a@]" FunctionType.pp mf_type;
+      List.iter ~f:(fprintf ff "@;%a" Instruction.pp) mf_body;
+      fprintf ff "@[)@]@]"
   end
 
   module Global = struct
@@ -302,7 +471,7 @@ module Module = struct
           mutability := Mutability.t;
           Core.coq_type := Type.t;
           Core.instruction := Instruction.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
   end
 
   module Import = struct
@@ -314,14 +483,14 @@ module Module = struct
             Core.function_type := FunctionType.t;
             Core.coq_type := Type.t;
             mutability := Mutability.t])]
-      [@@deriving show, eq, ord, iter, map, fold, sexp]
+      [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
     end
 
     type t =
       [%import:
         (Richwasm_extract.RwSyntax.module_import
         [@with module_import_desc := Desc.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
   end
 
   module Export = struct
@@ -330,14 +499,14 @@ module Module = struct
         [%import:
           (Richwasm_extract.RwSyntax.module_export_desc
           [@with Z.t := Big_int_Z.t])]
-      [@@deriving show, eq, ord, iter, map, fold, sexp]
+      [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
     end
 
     type t =
       [%import:
         (Richwasm_extract.RwSyntax.module_export
         [@with module_export_desc := Desc.t])]
-    [@@deriving show, eq, ord, iter, map, fold, sexp]
+    [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
   end
 
   type t =
@@ -349,5 +518,33 @@ module Module = struct
         module_function := Function.t;
         Z.t := Big_int_Z.t;
         module_export := Export.t])]
-  [@@deriving show, eq, ord, iter, map, fold, sexp]
+  [@@deriving eq, ord, iter, map, fold, sexp, show { with_path = false }]
+
+  let pp_sexp ff x = Sexp.pp_hum ff (sexp_of_t x)
+
+  let pp ff ({ m_imports; m_globals; m_funcs; m_table; m_start; m_exports } : t)
+      : unit =
+    let print_sep ~f ~sep lst =
+      List.iter
+        ~f:(fun x ->
+          sep ();
+          f x)
+        lst
+    in
+    let space_hint () = fprintf ff "@ " in
+    let break_hint () = fprintf ff "@;" in
+
+    fprintf ff "@[<v 2>@[(module @]";
+    print_sep ~f:(Import.pp ff) ~sep:break_hint m_imports;
+    print_sep ~f:(Global.pp ff) ~sep:break_hint m_globals;
+    print_sep ~f:(Function.pp ff) ~sep:break_hint m_funcs;
+    fprintf ff "@;@[(table@[<hv 2>";
+    if List.is_empty m_table then () else fprintf ff "@ ";
+    print_sep ~f:(Big_int_Z.pp ff) ~sep:space_hint m_table;
+    fprintf ff "@])@]";
+    Option.iter
+      ~f:(fun start -> fprintf ff "@;(start %a)" Big_int_Z.pp start)
+      m_start;
+    print_sep ~f:(Export.pp ff) ~sep:break_hint m_exports;
+    fprintf ff "@])@]"
 end
