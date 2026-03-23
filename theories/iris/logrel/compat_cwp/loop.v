10a33d98d894fb94c29a3481c546d4665e0709bd
Brianna Marshall <brianna@bmarsha.com>
2026-03-22 20:33:20 -0400

    Shorter proof of compat_loop

--- theories/iris/logrel/compat_cwp/loop.v
+++ theories/iris/logrel/compat_cwp/loop.v
@@ -42,18 +42,14 @@ Section Fundamental.
     ⊢ have_instr_type_sem rti sr mr M F L WT WL es' ψ L.
   Proof.
     iIntros (????? Hok IH Hcg ?????????) "@@@@@@@@@@@".
-    cbn in Hcg.
-    unfold compile_loop in Hcg.
     inv_cg_bind Hcg ?res ?wt ?wt ?wl ?wl ?es ?es ?Hcg ?Hcg.
     inv_cg_try_option Hcg.
     inv_cg_bind Hcg0 [[] ?res] ?wt ?wt ?wl ?wl ?es ?es ?Hcg ?Hcg.
     apply run_codegen_capture in Hcg as [Hcg ->].
     inv_cg_emit Hcg0.
-    clear Hretval.
     destruct res as [ts1 ts2].
     subst WT WL ψ.
-    rename Heq_some into Htrans.
-    apply bind_Some in Htrans as (ts1' & Htrans1 & Htrans).
+    apply bind_Some in Heq_some as (ts1' & Htrans1 & Htrans).
     apply bind_Some in Htrans as (ts2' & Htrans2 & Hts).
     inversion Hts.
     subst.
@@ -62,29 +58,18 @@ Section Fundamental.
     1, 2, 3: done.
     iDestruct (big_sepL2_length with "Hvs") as "%Hlen_vs".
     eapply IH in Hcg.
-    iApply (cwp_loop with "[$] [$] [Hvs Hos Hframe Hrt]").
-    - by eapply has_values_is_consts.
+    iApply (cwp_loop' with "[$] [$] [Hvs Hos Hframe Hrt]"); first done.
     - rewrite <- Hlen_ts1. rewrite Hlen_vs. by apply has_values_length.
-    - iIntros "!> Hfr Hrun".
-      iApply (Hcg with "[] [] [$] [] [$] [$] [$] [$] [$] [$] [$]").
-      1, 2: done.
-      iApply labels_interp_cons.
-      1, 2, 3, 5: done.
-      iModIntro.
-      iIntros (??) "(Hframe & %os' & %θ' & Hos' & Hvs' & Hrt)".
-      instantiate (1 := fun fr' vs' =>
+    - instantiate (1 := fun fr' vs' =>
         (frame_interp rti sr se L (wl ++ wl2 ++ wlf) inst fr' ∗
            ∃ os' θ', values_interp rti sr se τs1 os' ∗ atoms_interp os' vs' ∗ rt_token rti sr θ')%I).
       iFrame.
-    - do 2 iModIntro.
-      iIntros (??) "Hfr Hrun (Hframe & %os' & %θ' & Hos & Hvs & Hrt)".
-      iApply (Hcg with "[] [] [$] [] [$] [$] [$] [$] [$] [$] [$]").
-      + done.
+    - iIntros "!> !> %fr' %vs' Hfr Hrun (Hframe & %os' & %θ' & Hos & Hvs & Hrt)".
+      iApply (Hcg with "[] [] [$] [] [$] [$] [$] [$] [$] [$] [$]"); first done.
       + iPureIntro. apply has_values_to_consts.
       + iApply labels_interp_cons.
         1, 2, 3, 5: done.
-        iModIntro.
-        iIntros (??) "(Hframe & %os'' & %θ'' & Hos & Hvs & Hrt)".
+        iIntros "!> %fr'' %vs'' (Hframe & %os'' & %θ'' & Hos & Hvs & Hrt)".
         iFrame.
   Qed.
 
