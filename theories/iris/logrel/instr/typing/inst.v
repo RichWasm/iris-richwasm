144afff4813efd477b3f7bb0e7f48325bf6ee098
Elan <sysemenova@gmail.com>
2026-04-16 12:07:10 -0400

    inst progress

--- theories/iris/logrel/instr/typing/inst.v
+++ theories/iris/logrel/instr/typing/inst.v
@@ -25,6 +25,60 @@ Section inst.
     run_codegen (compile_instr mr fe (IInst ψ ix)) wt wl = inr ((), wt', wl', es') ->
     ⊢ have_instr_type_sem rti sr mr M F L WT WL lmask es' ψ L.
   Proof.
+    intros fe WT WL lmask κ ψ Hfinst Hok Hcg.
+    cbn in Hcg; inversion Hcg; subst wt' wl' es'; clear Hcg.
+
+    iIntros (??????????) "@@@@@@@@@@".
+    clear_nils.
+
+    iApply (cwp_val with "[$Hfr] [$Hrun]"); [apply H0|].
+    iSplitR; auto.
+    iFrame.
+    iPoseProof (values_interp_one_eq with "Hos") as "Hos".
+    iPoseProof (value_interp_coderef with "Hos") as "%Hos".
+    destruct Hos as (n32 & ->).
+    iApply values_interp_one_eq.
+    iApply value_interp_eq.
+    iPoseProof (value_interp_eq with "Hos") as "Hos".
+
+    (* now we need to use the key hypothesis: Hfinst *)
+    destruct Hfinst.
+
+    (* dig into all at once down to closure interp *)
+    all: unfold ϕ'; cbn.
+
+    all: iDestruct "Hos" as "(%κ' & %toinvert & HKindInterp & Rest)".
+    all: inversion toinvert; subst; clear toinvert.
+
+    all: iExists (SVALTYPE [I32R] NoRefs).
+    all: iFrame.
+    all: iSplitR; auto.
+
+    all: iDestruct "Rest" as
+      "(%n & %n32subst & %j & %cl & %HRepr & %toinvert &
+          Hclosure & Hwt & Hwf)".
+    all: inversion toinvert; subst n32subst; clear toinvert.
+
+    all: iExists n, n32.
+    all: iExists j, cl.
+    all: iFrame.
+    all: iSplitR; auto; iSplitR; auto.
+
+    (* these will likely have to be done separately. at the end maybe
+       pull together better *)
+    - destruct μ.
+      + iEval (cbn).
+        (* skipping for now *)
+        admit.
+      + iSpecialize ("Hclosure" $! b).
+
+        admit.
+    all: admit.
+
+
+    
+
+
     (* intros fe WT WL κ ψ Hfinst Hok Hcg. *)
     (* cbn in Hcg; inversion Hcg; subst wt' wl' es'; clear Hcg. *)
     (* simpl to_e_list. *)
