7fe4ad66aa0aa0b77aa8864c5b6ec5eb8aea67e4
Elan <sysemenova@gmail.com>
2026-04-25 13:25:04 -0400

    more bones for call

--- theories/iris/logrel/instr/typing/call.v
+++ theories/iris/logrel/instr/typing/call.v
@@ -13,41 +13,75 @@ Section call.
   Variable sr : store_runtime.
   Variable mr : module_runtime.
 
-  Lemma closure_cant_be_func_host :
-    ∀ ϕ se ft ix,
-      (* function_type_insts F ixs ϕ ϕ' -> *)
-      closure_interp0 rti sr (value_interp rti sr) se ϕ (FC_func_host ft ix) -∗ False.
+  (* Note: there's a lot more we know about se', τs1, and τs2 *)
+  (* I'll figure out what I need as I go *)
+  Lemma unravel_closure_interp :
+    ∀ F ixs τs1_s τs2_s ϕ se se_cl cl,
+      sem_env_interp (Σ:=Σ) F se ->
+      function_type_insts F ixs ϕ (MonoFunT τs1_s τs2_s) ->
+      closure_interp0 rti sr (value_interp rti sr) se_cl ϕ cl -∗
+      (∃ se' τs1 τs2, mono_closure_interp0 rti sr (value_interp rti sr) se' τs1 τs2 cl).
   Proof.
-    induction ϕ.
-    - by iIntros.
-    - iIntros (???) "#Hcl".
-      cbn.
-      iSpecialize ("Hcl" $! MemMM).
-      iPoseProof (IHϕ (senv_insert_mem MemMM se) ft ix) as "IHϕ".
-      iApply IHϕ; done.
-    - iIntros (???) "#Hcl".
-      cbn.
-      iSpecialize ("Hcl" $! []).
-      iPoseProof (IHϕ (senv_insert_rep [] se) ft ix) as "IHϕ".
-      iApply IHϕ; done.
-    - iIntros (???) "#Hcl".
-      cbn.
-      iSpecialize ("Hcl" $! 42).
-      iPoseProof (IHϕ (senv_insert_size 42 se) ft ix) as "IHϕ".
-      iApply IHϕ; done.
+    intros.
+    generalize dependent se.
+    generalize dependent se_cl.
+    remember (MonoFunT τs1_s τs2_s) as ϕ'.
+    induction H0.
+    - subst.
+      intros.
+      iIntros "#Hcl".
+      unfold closure_interp0.
+      iExists se_cl, τs1_s, τs2_s.
+      done.
+    - destruct ϕ.
+      + intros.
+        iIntros "#Hcl".
+        inversion H.
+      + intros.
+        iIntros "#Hcl".
+        inversion H; subst.
+        (* this apply chooses the wrong se' *)
+        iApply IHfunction_type_insts; auto.
+        (* now this is substitution lemmas *)
+        all: admit.
+      + admit.
+      + admit.
+      + intros.
+        iIntros "#Hcl".
+        inversion H; subst.
+        unfold closure_interp0.
+        (* now I need a sκ and T st
+           eval_kind se k = sκ   ==   Heval
+           skind_interp sκ T     ==   Hskind
+         *)
 
-    - iIntros (???) "#Hcl".
-      cbn.
-      (* okay yes but idk how to pull a T out of my hat *)
-      (* or an sκ for that matter *)
-      (* i have at least convinced myself that closure interp
-         100% only works over  FC_func_native
-       *)
-      (* if that isn't the case this will be interesting *)
-      (* also I think I get the k and sκ out of
-         function_type_insts, but the induction got weird
-       *)
+        apply has_kind_inv in H7 as Hkok.
+        inversion Hkok; subst.
+        apply (eval_kind_ok_Some _ _ _ H1) in H3 as Heval.
+        inversion Heval; rename x into sκ; subst.
+        apply (kinding_sound rti sr mr _ _ _ _ _ H7 H1) in H4 as Hskind.
+        iSpecialize ("Hcl" $! sκ (value_interp rti sr se τ)).
+
+        (* bad things with se_cl. induction hates me *)
+        (* iApply IHfunction_type_insts; auto; last first. *)
+        admit.
+
   Admitted.
+
+  Lemma closure_cant_be_func_host :
+    ∀ F ixs τs1_s τs2_s ϕ se se_cl ft ix,
+      sem_env_interp (Σ:=Σ) F se ->
+      function_type_insts F ixs ϕ (MonoFunT τs1_s τs2_s) ->
+      closure_interp0 rti sr (value_interp rti sr) se_cl ϕ (FC_func_host ft ix) -∗ False.
+  Proof.
+    intros.
+    iIntros "#Hcl".
+    pose proof (unravel_closure_interp _ _ _ _ _ se se_cl (FC_func_host ft ix) H H0).
+    iPoseProof (H1 with "[$Hcl]") as "Hcl2".
+    iEval (cbn) in "Hcl2".
+    iDestruct "Hcl2" as "(%a & %b & %c & F)".
+    done.
+  Qed.
 
   Lemma compat_call M F L wt wt' wtf wl wl' wlf es' i ixs ϕ τs1 τs2 :
     let fe := fe_of_context F in
@@ -101,9 +135,11 @@ Section call.
      *)
     iEval (cbn) in "Hcl".
     destruct cl as [inst ft ts es | f1 f2];
-      [|iPoseProof (closure_cant_be_func_host with "[$Hcl]") as "[]"].
+      [|iPoseProof (closure_cant_be_func_host with "[$Hcl]") as "[]"; done].
     destruct ft as [ts1 ts2].
     iEval (cbn) in "Hcl".
+
+
 
     (* okay so length evs = length ts1 is going to be weird *)
     (* it's going to be a weird combo of closure interp to get
@@ -114,6 +150,24 @@ Section call.
 
        I'll admit for now
      *)
+
+    (* the translate types and everything will come out of mono_function_interp
+       the mono_function_interp will come out of unravel_closure_interp
+       however, that's currently not giving us enough info for the length thing.
+       I'll add that later
+     *)
+
+    iPoseProof (unravel_closure_interp _ _ _ _ _ _ _ _ H Hfuntype with "[$Hcl]")
+      as "#Hcl2".
+    iDestruct "Hcl2" as "(%se' & %τs1_s & %τs2_s & #Hcl2 )".
+    iDestruct "Hcl2" as "(%Htrans1 & %Htrans2 & Hcl2)".
+    unfold mono_closure_interp0.
+
+    (* here you can see that we'll need translate_types and some
+       connection between τs1_s and τs1. That will need to happen through
+       unravel_closure_interp being better
+     *)
+
 
     (* before applying, open invariant *)
     iApply fupd_cwp.
@@ -135,11 +189,38 @@ Section call.
       iMod ("Hclose1" with "[$Hown $nsfun1]") as "Hown".
       iModIntro.
 
-      (* okay. This will be gotten out of the closure interp, Hcl *)
-      (* but. there's an ixs number of ∀ μ ρ κ ... in the way *)
-      (* hm... *)
+      iPoseProof ("Hcl2" with "[$Hvs] [Hos] [$Hrt] [$Hown] [$Hfr] [$Hrun]") as "Hcwp".
+      {
+        (* this will rely on more connections between se<->se' and τs1<->τs1_s
+           this will also have to be added/gotten from unravel_closure_interp
+         *)
+        admit.
+      }
 
-
+      iApply (cwp_frame_ctx1 with "[Hcwp] [Hframe]").
+        { iApply "Hcwp". }
+        { iApply "Hframe". }
+        { iIntros (??) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)". iFrame.
+          iSplitR; auto.
+          (* here's the necessary τs2<->τs2_s connection *)
+          admit.
+        }
+        { iIntros (?) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)". iFrame.
+          iSplitR; auto.
+          (* same as above *)
+          admit.
+        }
+        {
+          iIntros (f vs0) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)".
+          iFrame.
+          (* here we need same as above, but also the same reasoning as for the
+             previous length based goal but between vs0?
+             ah yes this will similarly go through translate_types/whatever added onto
+             unravel_closure_interp and atoms_interp
+             so same as above but slightly more involved
+           *)
+          admit.
+        }
   Admitted.
 
 End call.
