41a8dcd6a1f02861e220a0036b250a29ce668be7
Elan <sysemenova@gmail.com>
2026-06-03 15:53:06 -0400

    start of store weak

--- theories/iris/logrel/instr/typing/store_weak.v
+++ theories/iris/logrel/instr/typing/store_weak.v
@@ -1,4 +1,6 @@
 Require Import RichWasm.iris.logrel.instr.typing.common.
+Require Import RichWasm.iris.logrel.case_ptr.
+Require Import RichWasm.iris.numerics.
 
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
@@ -35,6 +37,15 @@ Section store_weak.
       inversion Heqref; eauto.
   Qed.
 
+  (* TEMPORARY. COPIED FROM LOAD_COPY *)
+  Lemma atom_interp_ptr_shaped ptr v :
+    atom_interp (PtrA ptr) v -∗
+    ∃ n n32, ⌜N_i32_repr n n32⌝ ∗
+             ⌜v = VAL_int32 n32⌝ ∗
+             ⌜ptr_shaped ptr n⌝ ∗
+             ∃ rp, ⌜repr_root_pointer rp n⌝ ∗ root_pointer_interp rp ptr.
+  Proof. Admitted.
+
   Lemma compat_store_weak M F L wt wt' wtf wl wl' wlf es' κ κser μ τ τval π pr :
     let fe := fe_of_context F in
     let WT := wt ++ wt' ++ wtf in
@@ -53,13 +64,14 @@ Section store_weak.
     unfold WT, WL; clear WT WL.
     cbn in Hcompile.
 
+    (** COMPILER DESTRUCTION **)
     inv_cg_bind Hcompile ρ ?wt ?wt ?wl ?wl es_off ?es_rest Hρ Hcompile.
     inv_cg_bind Hcompile ιs ?wt ?wt ?wl ?wl es_ρ ?es_rest Hιs Hcompile.
     inv_cg_bind Hcompile off ?wt ?wt ?wl ?wl  es_fail ?es_rest Hoff Hcompile.
     inv_cg_try_option Hρ; rename Heq_some into Hρ.
     inv_cg_try_option Hιs; rename Heq_some into Hιs.
     inv_cg_try_option Hoff; rename Heq_some into Hoff.
-    inv_cg_bind Hcompile lcidxs ?wt ?wt ?wl ?wl  es_save ?es_rest Hsave Hcompile.
+    inv_cg_bind Hcompile val_localidxs ?wt ?wt wl_save ?wl  es_save ?es_rest Hsave Hcompile.
     inv_cg_bind Hcompile a ?wt ?wt ?wl ?wl  es_a ?es_rest Ha Hcompile.
     cbn in Ha; inversion Ha; subst; clear Ha.
     inv_cg_bind Hcompile res_emit ?wt ?wt ?wl ?wl  es_emit ?es_rest Hemit Hcompile.
@@ -71,9 +83,18 @@ Section store_weak.
     clear_nils.
     destruct u. destruct p; destruct u; destruct u0.
 
-    (* Let's dig into some of the atoms/values that are floating around *)
+    (* Summary: (put any useful info about the variables here. probs obvious stuff)
+       - wl_save is the list of locals associated with the thing we're storing into memory
+     *)
+
+
+
+    (** OUTERMOST DIGGING **)
     unfold have_instr_type_sem.
     iIntros (??????????) "@@@@@@@@@@".
+
+    (* we have two things on the stack: a reference and a τval *)
+    (* separate the value interp *)
     iPoseProof (values_interp_cons_l with "[$Hos]") as "(%os1 & %os2 & -> & Hos1 & Hos2)".
     iEval (rewrite values_interp_one_eq; cbn) in "Hos2".
     iPoseProof (atoms_interp_app_l with "[$Hvs]") as "(%vs1 & %vs2 & -> & Hvs1 & Hvs2)".
@@ -82,134 +103,289 @@ Section store_weak.
     rename a into o1.
     iEval (rewrite atoms_interp_one_inv) in "Hvs1".
     iDestruct "Hvs1" as "(%v1 & -> & Hv1)".
-    set (ptr_local := sum_list_with length F.(typing.fc_locals) + length (wl ++ wl6) ) in *.
-    iDestruct "Hos1" as (κ' Hκ') "[Harepos1 Hrefos1]".
-    destruct κ'; [|by iDestruct "Harepos1" as "[[] ?]"].
-    iDestruct "Harepos1" as "%Harepos1".
-    cbn in Hκ'.
+    clear Hos1len.
 
-
-    assert (Hκ: eval_rep se (AtomR PtrR) = Some l).
-    {
-      destruct Htype as [Hmono Hctx].
-      destruct Hmono as [Hmono _].
-      rewrite Forall_cons in Hmono.
-      destruct Hmono as [Hmono Hmonoτval].
-      rewrite Forall_singleton in Hmonoτval.
-      destruct Hmono as (? & Hrep & Hismono).
-      inversion Hrep; subst.
-      apply rep_ref_kind_ptr_TEMP in H1; subst.
-      destruct H1 as [-> [χ' ->]].
-      unfold eval_kind in Hκ'.
-      apply bind_Some in Hκ'; destruct Hκ' as [l' [Heval Hret]].
-      inversion Hret; subst; auto.
-    }
-    cbn in Hκ; inversion Hκ; subst l.
-    unfold has_areps in Harepos1.
-    destruct Harepos1 as [[os1' [Hos1 Harepsos1]] Hrefflag].
-    inversion Hos1; subst os1'; clear Hos1. clear Hκ.
-    apply Forall2_cons_1 in Harepsos1 as [Harepo1 _].
-    destruct o1; inversion Harepo1.
-    apply has_values_app_inv in H0 as (evs1 & evs2 & Hevs & Hevs1 & Hevs2).
-    pose proof (has_values_length _ _ Hevs1) as Hlenevs1.
-    destruct evs1 as [|ev1 [|evvv' evvvs]]; try (cbn in *; congruence).
-    iPoseProof (frame_interp_wl_interp with "Hframe") as "%Hwl".
-    subst.
-    (* clearing some useless things *)
-    clear Hlenevs1 Hos1len.
-    rewrite (app_assoc ([ev1] ++ evs2)).
-    iApply (cwp_seq with "[Hfr Hrun]").
-    {
-      rewrite <- (app_assoc [ev1]).
-     (* instantiate (1 := λ f vs, (
-        ⌜vs = [v1]⌝ ∗
-        ⌜∀ i, i ∉ lcidxs -> f_locs f !! localimm i = f_locs fr !! localimm i⌝ ∗
-        ⌜Forall2 (fun i v => f_locs f !! localimm i = Some v) lcidxs vs2⌝
-        )%I).*)
-      instantiate (1 := λ fr' vs, (
-        ∃ val_idxs,
-        ⌜vs = [v1]⌝ ∗
-        ⌜frame_rel (λ i, i ∉ val_idxs) fr fr'⌝ ∗
-        ⌜Forall2 (fun i v => f_locs fr' !! localimm i = Some v) lcidxs vs2⌝ ∗
-        ⌜val_idxs = seq (fe_wlocal_offset fe + length wl) (length wl6)⌝ ∗
-        ⌜lcidxs = map prelude.W.Mk_localidx val_idxs⌝
-        )%I).
-
-      iApply cwp_val_app; first done.
-      set (temp := map prelude.W.Mk_localidx
-                        (seq (fe_wlocal_offset fe + length wl)
-                           (length (map translate_prim (map arep_to_prim ιs))))) in *.
-
-      eapply cwp_save_stack_w in Hsave.
-      4: {
-        apply Hevs2.
-      }
-      2: apply Hwl.
-      + destruct Hsave as (-> & -> & -> & Hsave).
-        (*iApply (Hsave with "[$] [$]").
-        iIntros (f' [Hfsame Hfchanged]).
-        unfold fvs_combine; done.*) admit.
-      + admit.
-      + admit.
-    }
-
-    iIntros (fr_saved w)
-      "(%val_idxs & -> & %Hfrel_fr_saved & %Hsaved & %Hval_idxs_seq & %Hval_localidxs) Hfr Hrun".
-
-    assert (ptr_local < length (f_locs fr_saved)) as Hlen.
-    {
-      admit.
-    }
-
-
-    simpl to_consts. iEval (rewrite app_assoc).
-
-    iApply (cwp_seq with "[Hfr Hrun]").
-    {
-      simpl app.
-      iApply (cwp_local_tee with "[ ] [$] [$]"); first auto.
-      admit.
-    }
-
-    (*
-    iIntros (f0 vs0) "[-> ->] Hfr Hrun".
-    eapply cwp_case_ptr in Hcompile.
-    2: do 2 instantiate (1 := []).
-    2,3: done.
-    destruct Hcompile as (?wt & ?wt & ?wt & ?wl & ?wl & ?wl & ?es & ?es & ?es & Hcompile).
-    destruct Hcompile as (Hunr & Hload1 & Hload2 & Hwt0 & Hwl0 & Hspec).
-
-
-
-
-    iApply cwp_val_app.
-    { instantiate (1 := [v1]).
-      apply Is_true_true.
-      apply/andP; split => //. by apply/eqP. }
-
-    iApply (cwp_seq with "[Hfr Hrun Hv1 Hrefos1]").
-    {
-      iApply (Hspec with "[$] [$] [] [$Hv1]").
-      {
-        iPureIntro; cbn. rewrite list_lookup_insert. by rewrite decide_True.
-      }
-      iIntros "!> Hfr Hrun Hv1".
-      clear_nils.
-      iEval (cbn) in "Hrefos1".
-      destruct μ as [|[|]]; first done.
-      - (* mm *)
-        unfold ref_mm_interp.
-        admit.
-      - (* gc *)
-        unfold ref_gc_interp.
-        admit.
-
-    }
-
-    iIntros (ff vss) "hi Hfr Hrun".
-    subst.
+    (* Summary:
+       - o1 is the atom associated with the ref, and v1 is its associated value
+       - os2 are the atoms associated with τval, and vs2 are its values
+       Note: o1 is Ptr _, but it's easier to get that after splitting between MM
+       and GC.
      *)
 
+    (* Set any other useful keys here? *)
+    set (ptr_local := sum_list_with length F.(typing.fc_locals) + length (wl ++ wl_save) ) in *.
+
+
+
+    (** KINDING STUFF *)
+
+
+
+    (** OTHER GENERAL FACTS THAT WE NEED *)
+    iPoseProof (frame_interp_wl_interp with "Hframe") as "%Hwl". (* for saving stack *)
+
+
+
+
+    (** BEGIN THE SPLIT **)
+
+    (* This is where it's best to show o1 is ptr and v1 a ptr shaped number,
+       so the first bit of both sections is dedicated to that.
+       this bit first for that `last done` in the splitting line.
+     *)
+    rewrite value_interp_eq.
+    iDestruct "Hos1" as (sκ Hsκ) "[%skindsv Ho1]".
+    iEval (cbn) in "Ho1".
+
+    destruct (eval_mem se μ) eqn:evalμ; last done; destruct b.
+    1: refine ?[MemMM]. 2: refine ?[MemGC].
+
+    [MemMM]: {
+      iEval (cbn) in "Ho1".
+      iDestruct "Ho1" as "(%ℓ & %fs & %ws & %toinvert & Hℓ_layout & Hℓ_heap & Hτ)".
+      inversion toinvert; subst; clear toinvert.
+      iPoseProof (atom_interp_ptr_shaped with "Hv1") as
+        "(%n & %n32 & %Hn32 & -> & %Hnshp & %rp & %Hreproot & Hv1)".
+      apply has_values_app_inv in H0.
+      destruct H0 as (ev1 & evs2 & -> & Hev1 & Hevs2).
+      apply has_values_to_consts_inv in Hev1 as Hev1tosubst.
+      cbn in Hev1tosubst; subst.
+
+      (* Summary:
+         - o1 became (PtrHeap MemMM ℓ), v1 became (VAL_int32 n32), ev1 became BI_const...
+         - n32 is... the address associated with the pointer? Or at minimum it is the
+           actual thing on the stack.
+         - we also dug into the value interp of the reference, getting the following:
+           + we got rp, the root pointer associated with ℓ, and a bunch of ℓ resources
+           + we also importantly got the type interpretation of τ (what's currently being
+             pointed to by the reference) and the words ws currently sitting there
+       *)
+
+
+      (** PUT FACTOIDS WE NEED HERE (THAT ARE MM/GC SPECIFIC) **)
+
+
+
+      (** TIME TO BOOGIE *)
+      (* note: I think saving stack and local tee can happen before the split *)
+      (* it's a little easier to already have v1 being an n32 though *)
+
+      (** SAVING STACK AND LOCAL TEE *)
+      (* First, saving stack to clear evs2 and es_save *)
+
+      (* apply lemma on the codegen. order of goals to help with evars *)
+      eapply cwp_save_stack_w in Hsave; auto.
+      4: exact Hevs2.
+      3: {
+        (* About result_type_interp_of_atoms_interp. *)
+        (* true but boring/later *)
+        admit.
+      }
+      2: exact Hwl.
+      destruct Hsave as (Hval_localidxs_seq & -> & Hwl_save & Hsave).
+
+      (* now to use the facts we got *)
+      rewrite (app_assoc _ es_save _).
+      rewrite <- (app_assoc _ evs2 _).
+      iApply (cwp_seq with "[Hfr Hrun]").
+      {
+        (* note: this is 100% copied from case.v lol *)
+        (* it looks like this is all just related to cwp_save_stack so should be the same *)
+        (* possibility of it being incorrect *)
+        instantiate (1 := λ fr' vs, (
+          ∃ val_idxs,
+          ⌜vs = [VAL_int32 n32]⌝ ∗
+          ⌜frame_rel (λ i, i ∉ val_idxs) fr fr'⌝ ∗
+          ⌜Forall2 (fun i v => f_locs fr' !! localimm i = Some v) val_localidxs vs2⌝ ∗
+          ⌜val_idxs = seq (fe_wlocal_offset fe + length wl) (length wl_save)⌝ ∗
+          ⌜val_localidxs = map prelude.W.Mk_localidx val_idxs⌝
+                                    )%I).
+        iApply cwp_val_app; first done.
+        iApply (Hsave with "[$] [$]").
+        iIntros (f' [Hfsame Hfchanged]).
+        unfold fvs_combine.
+        subst val_localidxs wl_save.
+        auto.
+      }
+      iIntros (fr_saved w) "(%val_idxs & -> & %Hfrel_fr_saved & %Hsaved & %Hval_idxs_seq & %Hval_localidxs) Hfr Hrun".
+      clear Hsave.
+
+      (* Next: local_tee stuff *)
+      set (f' := ({|
+                     W.f_locs :=
+                       <[localimm (prelude.W.Mk_localidx ptr_local):=
+                           VAL_int32 n32]> (f_locs fr_saved);
+                     W.f_inst := f_inst fr_saved
+                   |})) in *.
+      rewrite (app_assoc (to_consts [VAL_int32 n32]) _ _).
+      iApply (cwp_seq with "[Hfr Hrun]").
+      {
+        (* this is copied from load.v *)
+        iApply (cwp_local_tee with "[] [$] [$]").
+        - admit. (* this probably needs more work? *)
+        - now instantiate (1:= λ f'' v'', ⌜f'' = f' /\ v'' = [VAL_int32 n32]⌝%I).
+      }
+      iIntros (? ?) "(-> & ->) Hf Hrun".
+
+      (* Summary:
+         - Used up evs2 and es_save_stack and "put" the pointer in front of es_case_ptr block
+         - Our frame changed twice: fr_saved is after saving the stack (so lots of things)
+           got put into locals, and f' is after putting n32 into the local associated
+           with the ptr.
+         - We also got a bunch of val_indx stuffs everywhere
+
+         Note: probably put lemmas and facts related to saving the stack here. Try to make
+         them lemmas, since the GC case also needs them.
+      *)
+
+
+      (** CASE PTR TIME **)
+      (* Apply the lemma into the codegen *)
+      apply cwp_case_ptr in Hcompile.
+      destruct Hcompile as
+        (wt_unreach & wt_memMM & wt_memGC &
+           wl_unreach & wl_memMM & wl_memGC &
+           es_unreach & es_memMM & es_memGC & Hcompile).
+      destruct Hcompile as
+        (Hcg_unreach & Hcg_memMM & Hcg_memGC & -> & -> & Hcaseptr_spec).
+
+      (* Specialize the spec with out variables *)
+      specialize (Hcaseptr_spec [] []).
+      specialize (Hcaseptr_spec (PtrHeap MemMM ℓ) n n32).
+      specialize (Hcaseptr_spec ltac:(eauto) ltac:(auto) ltac:(auto) ltac:(auto)).
+      clear_nils.
+
+      (* A cwp_val_app, which I'm confused why it's on the stack at all but oh well *)
+      iApply cwp_val_app; first done.
+
+      (* Now we can cwp_seq and use the spec *)
+
+
+      admit.
+    }
+
+    [MemGC]: {
+      (* note: this first set up has a lot of copying *)
+      iEval (cbn) in "Ho1".
+      iDestruct "Ho1" as "(%ℓ & %fs & %toinvert & Hτ)".
+      inversion toinvert; subst; clear toinvert.
+      iPoseProof (atom_interp_ptr_shaped with "Hv1") as
+        "(%n & %n32 & %Hn32 & -> & %Hnshp & %rp & %Hreproot & Hv1)".
+      apply has_values_app_inv in H0.
+      destruct H0 as (ev1 & evs2 & -> & Hev1 & Hevs2).
+      apply has_values_to_consts_inv in Hev1 as Hev1tosubst.
+      cbn in Hev1tosubst; subst.
+
+      (* Summary:
+         - o1 became (PtrHeap MemGC ℓ), v1 became (VAL_int32 n32), ev1 became BI_const...
+         - n32 is... the address associated with the pointer? Or at minimum it is the
+           actual thing on the stack.
+         - we also dug into the value interp of the reference, getting the following:
+           + we got rp, the root pointer associated with ℓ, and a bunch of ℓ resources
+             hidden underneath an invariant
+           + we also got the type interpretation of the words sitting in memory under an
+             invariant
+       *)
+
+
+      (** PUT FACTOIDS WE NEED HERE (THAT ARE MM/GC SPECIFIC) **)
+
+
+
+      (** TIME TO BOOGIE *)
+
+      (** SAVING STACK AND LOCAL TEE *)
+      (* First, saving stack to clear evs2 and es_save *)
+
+      (* apply lemma on the codegen. order of goals to help with evars *)
+      eapply cwp_save_stack_w in Hsave; auto.
+      4: exact Hevs2.
+      3: {
+        (* About result_type_interp_of_atoms_interp. *)
+        (* true but boring/later *)
+        admit.
+      }
+      2: exact Hwl.
+      destruct Hsave as (Hval_localidxs_seq & -> & Hwl_save & Hsave).
+
+      (* now to use the facts we got *)
+      rewrite (app_assoc _ es_save _).
+      rewrite <- (app_assoc _ evs2 _).
+      iApply (cwp_seq with "[Hfr Hrun]").
+      {
+        (* note: this is 100% copied from case.v lol *)
+        (* it looks like this is all just related to cwp_save_stack so should be the same *)
+        (* possibility of it being incorrect *)
+        instantiate (1 := λ fr' vs, (
+          ∃ val_idxs,
+          ⌜vs = [VAL_int32 n32]⌝ ∗
+          ⌜frame_rel (λ i, i ∉ val_idxs) fr fr'⌝ ∗
+          ⌜Forall2 (fun i v => f_locs fr' !! localimm i = Some v) val_localidxs vs2⌝ ∗
+          ⌜val_idxs = seq (fe_wlocal_offset fe + length wl) (length wl_save)⌝ ∗
+          ⌜val_localidxs = map prelude.W.Mk_localidx val_idxs⌝
+                                    )%I).
+        iApply cwp_val_app; first done.
+        iApply (Hsave with "[$] [$]").
+        iIntros (f' [Hfsame Hfchanged]).
+        unfold fvs_combine.
+        subst val_localidxs wl_save.
+        auto.
+      }
+      iIntros (fr_saved w) "(%val_idxs & -> & %Hfrel_fr_saved & %Hsaved & %Hval_idxs_seq & %Hval_localidxs) Hfr Hrun".
+      clear Hsave.
+
+      (* Next: local_tee stuff *)
+      set (f' := ({|
+                     W.f_locs :=
+                       <[localimm (prelude.W.Mk_localidx ptr_local):=
+                           VAL_int32 n32]> (f_locs fr_saved);
+                     W.f_inst := f_inst fr_saved
+                   |})) in *.
+      rewrite (app_assoc (to_consts [VAL_int32 n32]) _ _).
+      iApply (cwp_seq with "[Hfr Hrun]").
+      {
+        (* this is copied from load.v *)
+        iApply (cwp_local_tee with "[] [$] [$]").
+        - admit. (* this probably needs more work? *)
+        - now instantiate (1:= λ f'' v'', ⌜f'' = f' /\ v'' = [VAL_int32 n32]⌝%I).
+      }
+      iIntros (? ?) "(-> & ->) Hf Hrun".
+
+      (* Summary:
+         - Used up evs2 and es_save_stack and "put" the pointer in front of es_case_ptr block
+         - Our frame changed twice: fr_saved is after saving the stack (so lots of things)
+           got put into locals, and f' is after putting n32 into the local associated
+           with the ptr.
+         - We also got a bunch of val_indx stuffs everywhere
+
+         Note: probably put lemmas and facts related to saving the stack here.
+      *)
+
+
+      (** CASE PTR TIME **)
+      (* Apply the lemma into the codegen *)
+      apply cwp_case_ptr in Hcompile.
+      destruct Hcompile as
+        (wt_unreach & wt_memMM & wt_memGC &
+           wl_unreach & wl_memMM & wl_memGC &
+           es_unreach & es_memMM & es_memGC & Hcompile).
+      destruct Hcompile as
+        (Hcg_unreach & Hcg_memMM & Hcg_memGC & -> & -> & Hcaseptr_spec).
+
+      (* Specialize the spec with out variables *)
+      specialize (Hcaseptr_spec [] []).
+      specialize (Hcaseptr_spec (PtrHeap MemGC ℓ) n n32).
+      specialize (Hcaseptr_spec ltac:(eauto) ltac:(auto) ltac:(auto) ltac:(auto)).
+      clear_nils.
+
+      (* A cwp_val_app, which I'm confused why it's on the stack at all but oh well *)
+      iApply cwp_val_app; first done.
+
+      (* Now we can cwp_seq and use the spec *)
+
+
+      admit.
+    }
 
 
   Admitted.
