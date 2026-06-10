d8265b0fee395ccadd8d3eb9ad1bd0a6b811e569
Brianna Marshall <brianna@bmarsha.com>
2026-06-10 15:30:32 -0400

    More setup in fundamental_module

--- theories/iris/logrel/module.v
+++ theories/iris/logrel/module.v
@@ -1,10 +1,12 @@
 From iris.proofmode Require Export base proofmode classes.
 
-Require Import RichWasm.typing.
-Require Import RichWasm.compiler.module.
+From RichWasm Require Import syntax typing.
+From RichWasm.compiler Require Import prelude module.
 Require Import RichWasm.iris.host.iris_host.
 Require Import RichWasm.iris.rules.iris_rules.
 From RichWasm.iris Require Import logrel memory util.
+
+Module W := RichWasm.wasm.datatypes.
 
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
@@ -42,13 +44,45 @@ Section Fundamental.
     subst wt' imps'.
     clear Himps_try.
     apply bind_inr in Hm as ([wt' defs] & Hdefs & Hm).
+
+    set user_exps := map (user_export (funcimm mr.(mr_func_user))) m.(m_exports).
+    set table_exps := map table_export m.(m_table).
+    set start_func := table_start m.(m_table).
+    set start_idx := W.Mk_funcidx (funcimm mr.(mr_func_user) + length imps + length defs).
+    assert
+      (m' =
+         {|
+           W.mod_types := rt_types ++ wt ++ wt';
+           W.mod_funcs := defs ++ [start_func];
+           W.mod_tables := [];
+           W.mod_mems := [];
+           W.mod_globals := rt_globals;
+           W.mod_elem := [];
+           W.mod_data := [];
+           W.mod_start := Some (W.Build_module_start start_idx);
+           W.mod_imports := rt_imports ++ imps;
+           W.mod_exports := user_exps ++ table_exps
+         |}) as Hm' by by inversion Hm.
+    clear Hm.
+
     iApply (instantiation_spec_operational_start with "[$] [$] [-]").
+    - instantiate (2 := m'). by rewrite Hm'.
     - admit.
     - admit.
-    - admit.
-    - iFrame.
-      admit.
+    - iFrame. admit.
     - iIntros (?) "Hfr Hrun (Hmod & Himp & %inst & Hpost & Hhost)".
+      iDestruct "Hpost" as
+        "(% & % & % & % & % & % & Htypeck & %Hexps & %Htab_allocs & %Hwts' & %Helem_bound &
+          %Hmem_allocs & %Hwms' & %Hdata_bound & %Hglob_init & %Hglob_allocs & Hinst)".
+      destruct Hexps as
+        (Hinst_types & Hext_funcs & Hext_tab & Hext_mem & Hext_globs & Hcheck_start).
+      iDestruct "Hinst" as "(Hinst_func & Hinst_tab & Hinst_mem & Hinst_glob)".
+
+      rewrite Hm' in Hcheck_start.
+      cbn in Hcheck_start.
+
+      (* TODO: Find the ↦[wf] resource for idnstart. *)
+
       iApply wp_lift_wasm.
       change [AI_invoke idnstart] with ([] ++ [AI_invoke idnstart]).
       iApply (wp_invoke_native with "[$] [$]").
