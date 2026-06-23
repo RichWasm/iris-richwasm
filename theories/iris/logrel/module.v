ac4dea1b884401a95b0cf348756b4d28da39fb18
Ryan Doenges <me@ryandoeng.es>
2026-06-23 16:18:20 -0400

    start on module typing

--- theories/iris/logrel/module.v
+++ theories/iris/logrel/module.v
@@ -1,3 +1,5 @@
+From stdpp Require Import base.
+From mathcomp Require Import eqtype ssreflect.
 From iris.proofmode Require Export base proofmode classes.
 
 From RichWasm Require Import syntax typing.
@@ -31,6 +33,96 @@ Section Fundamental.
     - by intros (x & -> & Hfx).
   Qed.
 
+  Lemma check_start_Some m inst idx :
+    is_true (check_start m inst (Some idx)) →
+    ∃ ms msf,
+      mod_start m = Some ms /\
+      modstart_func ms = Mk_funcidx msf /\
+      nth_error (inst_funcs inst) msf = Some idx.
+  Proof.
+    cbn.
+    intros Hstart.
+    eapply ssrbool.elimT in Hstart; last apply opt_eqP.
+    rewrite bind_Some in Hstart.
+    destruct Hstart as (ms & Hms & Hlookup).
+    destruct (modstart_func ms) as [msf] eqn:?.
+    eauto.
+  Qed.
+
+  Definition mt_imps (mt : module_type) : list extern_t.
+  Admitted.
+
+  Definition mt_exps (mt : module_type) : list extern_t.
+  Admitted.
+
+  Definition translate_function_type : Core.function_type → function_type.
+  Admitted.
+
+  Lemma type_functions rt_types wt m wt' defs c table :
+    let ϕs := m_imports m ++ map mf_type (m_functions m) in
+    let M := {| mc_functions := ϕs; mc_table := table |} in
+    compile_functions (rt_types ++ wt) (m_functions m) = inr (wt', defs) →
+    Forall (λ mf : module_function, has_function_type M mf (mf_type mf)) (m_functions m) →
+    Forall2 (module_func_typing c) defs (map translate_function_type ϕs).
+  Proof.
+  Admitted.
+
+  Theorem type_module m m' mt :
+    has_module_type m mt ->
+    compile_module m = inr m' ->
+    module_typing m' (mt_imps mt) (mt_exps mt).
+  Proof.
+    iIntros (Hmt Hm).
+    apply bind_inr in Hm as ([wt imps] & Himps_try & Hm).
+    destruct (user_imports rt_types m.(module.m_imports)) as [[wt' imps']|] eqn:Himps;
+      last inversion Himps_try.
+    inversion Himps_try.
+    subst wt' imps'.
+    clear Himps_try.
+    apply bind_inr in Hm as ([wt' defs] & Hdefs & Hm).
+
+    set user_exps := map (user_export (funcimm mr.(mr_func_user))) m.(m_exports).
+    set table_exps := map table_export m.(m_table).
+    set start_func := table_start m.(m_table).
+    set start_idx' := funcimm mr.(mr_func_user) + length imps + length defs.
+    set start_idx := W.Mk_funcidx start_idx'.
+    assert
+      (m' =
+         {|
+           W.mod_types := rt_types ++ wt ++ wt';
+           W.mod_funcs := defs ++ [start_func];
+           W.mod_tables := [];
+           W.mod_mems := [];
+           W.mod_globals := rt_globals;
+           W.mod_elem := [];
+           W.mod_data := [];
+           W.mod_start := Some (W.Build_module_start start_idx);
+           W.mod_imports := rt_imports ++ imps;
+           W.mod_exports := user_exps ++ table_exps
+         |}) as Hm' by by inversion Hm.
+    clear Hm.
+
+    inversion Hmt; subst m0 mt.
+
+    do 2 eexists; rewrite Hm'; intros.
+    repeat split.
+    - instantiate (1 := map translate_function_type ϕs ++ [instruction.W.Tf [] []]).
+      apply Forall2_app.
+      + by eapply type_functions.
+      + constructor; last done.
+        unfold module_func_typing.
+        unfold start_func, table_start.
+        repeat split.
+        * admit.
+    - admit.
+    - done.
+    - done.
+    - admit.
+    - admit.
+    - admit.
+  Admitted.
+
+
   Theorem fundamental_module m m' mt :
     has_module_type m mt ->
     compile_module m = inr m' ->
@@ -48,7 +140,8 @@ Section Fundamental.
     set user_exps := map (user_export (funcimm mr.(mr_func_user))) m.(m_exports).
     set table_exps := map table_export m.(m_table).
     set start_func := table_start m.(m_table).
-    set start_idx := W.Mk_funcidx (funcimm mr.(mr_func_user) + length imps + length defs).
+    set start_idx' := funcimm mr.(mr_func_user) + length imps + length defs.
+    set start_idx := W.Mk_funcidx start_idx'.
     assert
       (m' =
          {|
@@ -67,9 +160,15 @@ Section Fundamental.
 
     iApply (instantiation_spec_operational_start with "[$] [$] [-]").
     - instantiate (2 := m'). by rewrite Hm'.
+    - instantiate (1 := []).
+      instantiate (1 := []).
+      do 2 eexists.
+      rewrite Hm'.
+      intros.
+      admit.
     - admit.
-    - admit.
-    - iFrame. admit.
+    - iFrame.
+      admit.
     - iIntros (?) "Hfr Hrun (Hmod & Himp & %inst & Hpost & Hhost)".
       iDestruct "Hpost" as
         "(% & % & % & % & % & % & Htypeck & %Hexps & %Htab_allocs & %Hwts' & %Helem_bound &
@@ -79,7 +178,30 @@ Section Fundamental.
       iDestruct "Hinst" as "(Hinst_func & Hinst_tab & Hinst_mem & Hinst_glob)".
 
       rewrite Hm' in Hcheck_start.
-      cbn in Hcheck_start.
+      eapply check_start_Some in Hcheck_start.
+      destruct Hcheck_start as (ms & msf & Hms & Hmsf & Hidnstartmsf).
+      cbn in Hms.
+      inversion Hms; subst ms; clear Hms.
+      inversion Hmsf; subst msf; clear Hmsf.
+
+      iEval (unfold module_inst_resources_func) in "Hinst_func".
+      iPoseProof (big_sepL2_lookup_acc with "Hinst_func") as "Hwfstart".
+      { admit. }
+      { instantiate (2 := funcimm (mr_func_user mr) + length defs).
+        instantiate (1 := idnstart).
+        rewrite lookup_drop.
+        unfold start_idx' in Hidnstartmsf.
+        rewrite -nth_error_lookup.
+        replace (get_import_func_count m' + (funcimm (mr_func_user mr) + length defs))
+          with (funcimm (mr_func_user mr) + length imps + length defs); first done.
+        rewrite Hm'.
+        rewrite Nat.add_assoc.
+        rewrite (Nat.add_comm _ (funcimm _)).
+        f_equal.
+        f_equal.
+        change @length with @seq.size.
+        admit. }
+
 
       (* TODO: Find the ↦[wf] resource for idnstart. *)
 
