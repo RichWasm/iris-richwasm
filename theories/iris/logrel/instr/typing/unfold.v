5fb620130c7b0d1aa030919e3c214ebb9b3d4b15
Elan <sysemenova@gmail.com>
2026-07-05 13:11:20 -0400

    unfold down to suspicious admits

--- theories/iris/logrel/instr/typing/unfold.v
+++ theories/iris/logrel/instr/typing/unfold.v
@@ -1,5 +1,6 @@
 Require Import RichWasm.iris.logrel.instr.typing.common.
 Require Import RichWasm.iris.logrel.instr.typing.inst. (* TODO: import proper subst file when created *)
+Require Import RichWasm.iris.logrel.env_props.
 
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
@@ -13,6 +14,27 @@ Section unfold.
   Variable rti : rt_invariant Σ.
   Variable sr : store_runtime.
   Variable mr : module_runtime.
+
+  (* copied: just because iApply was being annoying with doing the wrong
+   direction *)
+  Lemma type_interp_subst_type_forwards F F' se se' τ κ κ' sv sub_m sub_r sub_s sub_t :
+    let τ' := refresh_kinds F (subst_type sub_m sub_r sub_s sub_t τ) in
+    (sem_env_types_well_formed se') ->
+    (sem_env_types_well_formed se) ->
+    (sem_env_interp F' se') ->
+    (sem_env_interp F se) ->
+    (sem_env_rel_rep_eq se' se sub_r) ->
+    (sem_env_rel_size_eq se' se sub_s) ->
+    (sem_env_rel_mem_eq se' se sub_m) ->
+    (sem_env_rel_sκ_eq se' se sub_t) ->
+    (sem_env_rel_type_eq rti sr se' se sub_t) ->
+    (∀ i, refresh_kinds F (sub_t i) = sub_t i) ->
+    has_kind F' τ κ ->
+    has_kind F τ' κ' ->
+    (* type_eq_mod_kinds τ' (subst_type sub_m sub_r sub_s sub_t τ) -> *)
+    type_interp rti sr τ se' sv -∗
+    type_interp rti sr τ' se sv.
+  Proof. Admitted.
 
   Lemma compat_unfold M F L wt wt' wtf wl wl' wlf es' τ κ :
     let fe := fe_of_context F in
@@ -60,8 +82,51 @@ Section unfold.
     pose proof (fixpoint_unfold (skind_rec_interp1 sκ (type_interp rti sr τ) se)) as Hunf.
     specialize (Hunf (SAtoms os)).
     rewrite Hunf.
-    (* This looks like it should follow from a substitution lemma. *)
-    admit.
+    unfold skind_rec_interp1. iEval (cbn) in "Hos".
+    iModIntro.
+    inversion Hkind; subst.
+    assert (refresh_kinds F τrec = τrec). {
+      (* I need to prove has_kind F τrec for some κ'. subst κ? *)
+      (* I'm unsure if that's true, but maybe *)
+      admit.
+    }
+    unfold τrec in H0.
+    rewrite <- H0.
+    pose proof (sem_well_formed_from_interp F se Hse) as HseF.
+    iApply (type_interp_subst_type_forwards with "[$Hos]").
+    11: exact H4.
+    1-9: try done.
+    - unfold sem_env_types_well_formed in *.
+      apply Forall_cons; split; try exact HseF.
+      split; first apply subskind_of_refl.
+      (* RYAN: from what you know of kinding, is this true? *)
+      admit.
+    - apply sem_env_interp_insert_type; try done; first apply subskind_of_refl.
+      (* exactly the same as in the previous admit.  *)
+      admit.
+    - unfold sem_env_rel_sκ_eq.
+      intros i. destruct i; cbn; last apply subskind_of_option_refl.
+      rewrite Hκ. apply subskind_of_option_refl.
+    - unfold sem_env_rel_type_eq.
+      intros i; destruct i; cbn.
+      2: {
+        Opaque type_skind. Opaque skind_has_svalue.
+        rewrite value_interp_eq_no_sv; cbn.
+        Transparent type_skind. Transparent skind_has_svalue.
+        (* this is fine because of Hse relating snd snd se.2 to sk *)
+        admit.
+      }
+      (* hm this is suspicious. is this true? it's a different shape than
+        it used to be, so maybe it is *)
+      admit.
+    - intros i; destruct i; try done.
+      cbn.
+      pose proof (refresh_kinds_id rti sr mr) as (this & _).
+      apply this in H4.
+      rewrite <- H4.
+      done.
+    - (* this is whatever the kinding admit above is *)
+      admit.
   Admitted.
 
 End unfold.
