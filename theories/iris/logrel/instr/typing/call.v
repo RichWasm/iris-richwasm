18f54ee835147f678bd61211685c1cdc9558eaab
Elan <sysemenova@gmail.com>
2026-07-06 17:56:11 -0400

    call qed modulo has_kind_ft lemma

--- theories/iris/logrel/instr/typing/call.v
+++ theories/iris/logrel/instr/typing/call.v
@@ -1,4 +1,6 @@
 Require Import RichWasm.iris.logrel.instr.typing.common.
+Require Import RichWasm.iris.logrel.substitution.
+Require Import RichWasm.iris.logrel.env_props.
 
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
@@ -13,58 +15,191 @@ Section call.
   Variable sr : store_runtime.
   Variable mr : module_runtime.
 
+  (* a deeply critical lemma that should almost certainly be true *)
+  Lemma has_kind_ft_through_inst F ϕ ϕ' ix :
+    function_type_inst F ix ϕ ϕ' ->
+    has_kind_ft F ϕ ->
+    has_kind_ft F ϕ'.
+  Proof.
+    intros Hfinst Hkind.
+    induction Hfinst.
+    - inversion Hkind; subst; clear Hkind; rename H2 into Hkind.
+      constructor.
+      induction H.
+  Admitted.
+
   (* Note: there's a lot more we know about se', τs1, and τs2 *)
   (* I'll figure out what I need as I go *)
   Lemma unravel_closure_interp :
-    ∀ F ixs τs1_s τs2_s ϕ se se_cl cl,
+    ∀ F ixs τs1_s τs2_s ϕ se cl,
+      let ϕ' := InnerFunT (MonoFunT τs1_s τs2_s) in
       sem_env_interp (Σ:=Σ) F se ->
-      function_type_insts F ixs ϕ (InnerFunT (MonoFunT τs1_s τs2_s)) ->
-      closure_interp rti sr ϕ se_cl cl -∗
-      ∃ se' τs1 τs2,
+      function_type_insts F ixs ϕ ϕ' ->
+      has_kind_ft F ϕ ->
+      has_kind_ft F ϕ' ->
+      closure_interp rti sr ϕ se cl -∗
+        (* ϕ' = refresh_kinds_ft F (subst_function_type sub_m sub_r sub_s sub_t ϕ) /\ *)
         mono_closure_interp rti sr
-          τs1 τs2 (map (type_interp rti sr) τs1) (map (type_interp rti sr) τs2) se' cl.
+          τs1_s τs2_s (map (type_interp rti sr) τs1_s) (map (type_interp rti sr) τs2_s) se cl.
   Proof.
-    intros.
-    generalize dependent se.
-    generalize dependent se_cl.
+    intros *. intros Hse Hfinst Hkind_ϕ Hkind_ϕ'.
+    subst ϕ'.
     remember (InnerFunT (MonoFunT τs1_s τs2_s)) as ϕ'.
-    induction H0.
-    - subst.
-      intros.
-      iIntros "#Hcl".
-      iExists se_cl, τs1_s, τs2_s.
-      done.
-    - destruct ϕ.
-      + intros.
-        iIntros "#Hcl".
-        inversion H; subst.
-        (* now I need a sκ and T st
-           eval_kind se k = sκ   ==   Heval
-           skind_interp sκ T     ==   Hskind
-         *)
-        iApply IHfunction_type_insts; auto.
-        (* now this is substitution lemmas *)
-        all: admit.
-      + admit.
-      + admit.
-      + admit.
-
-  Admitted.
+    induction Hfinst; iIntros "Hcl";  subst.
+    - done.
+    - pose proof (has_kind_ft_through_inst _ _ _ _ H Hkind_ϕ) as Hkind_ϕ_middle.
+      inversion H; subst.
+      + inversion H0; subst.
+        assert (Hϕ': ϕ'0 = refresh_kinds_ift F (subst_inner_function_type VarM VarR VarS (unscoped.scons τ VarT) ϕ)). {
+          (* by hkind_ϕ_middle and refresh_kinds_eq_mod_kinds *)
+          pose proof (has_kind_ft_function_type_eq_mod_kinds rti sr mr) as (_ & _ & this).
+          eapply this; try done.
+          inversion Hkind_ϕ_middle; subst; done.
+        }
+        assert (∃ sκ, eval_kind se κ = Some sκ). {
+          apply has_kind_inv in H1 as Hokhas.
+          inversion Hokhas as [??? Hok_τ Hok_κ]; subst.
+          clear Hokhas.
+          destruct (eval_kind_ok_Some _ _ _ Hse Hok_κ) as [sκ_T Hsκ_T].
+          inversion H2; subst.
+          - cbn in Hsκ_T.
+            apply bind_Some in Hsκ_T as (ιs & evalρ & toinv).
+            inversion toinv; subst.
+            exists (SVALTYPE ιs ξ').
+            cbn.
+            rewrite evalρ.
+            cbn. done.
+          - cbn in Hsκ_T.
+            apply bind_Some in Hsκ_T as (ιs & evalρ & toinv).
+            inversion toinv; subst.
+            exists (SMEMTYPE ιs ξ').
+            cbn.
+            rewrite evalρ.
+            cbn. done.
+        }
+        destruct H4 as (sκ & evalκ).
+        clear H3.
+        rewrite closure_interp_eq. iEval (cbn -[senv_insert_type]) in "Hcl".
+        iPoseProof (inner_closure_interp_scons_insert_type rti sr mr _ _ _ _ _ with "[$Hcl]") as "Hcl2"; try done.
+        * rewrite <- Hϕ'.
+          inversion Hkind_ϕ_middle; subst. done.
+        * inversion Hkind_ϕ; subst.
+          inversion H5; subst.
+          done.
+        * specialize (IHHfinst ltac:(auto) eq_refl ltac:(auto) ltac:(auto)).
+          iApply IHHfinst.
+          rewrite closure_interp_eq.
+          cbn.
+          rewrite <- Hϕ'.
+          rewrite inner_closure_interp_eq.
+          done.
+      + rewrite closure_interp_eq. iEval (cbn -[senv_insert_mem]) in "Hcl".
+        pose proof (refresh_kinds_id) as (_ & this & _); try done.
+        apply this in Hkind_ϕ_middle as torew.
+        iDestruct "Hcl" as "#Hcl".
+        iPoseProof (closure_interp_scons_insert_mem rti sr mr _ _ with "[$Hcl]") as "Hcl2"; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          done.
+        * inversion Hkind_ϕ; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          specialize (IHHfinst ltac:(auto) eq_refl ltac:(auto) ltac:(auto)).
+          by iApply IHHfinst.
+      + rewrite closure_interp_eq. iEval (cbn -[senv_insert_rep]) in "Hcl".
+        pose proof (refresh_kinds_id) as (_ & this & _); try done.
+        apply this in Hkind_ϕ_middle as torew.
+        iDestruct "Hcl" as "#Hcl".
+        iPoseProof (closure_interp_scons_insert_rep rti sr mr _ _ with "[$Hcl]") as "Hcl2"; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          done.
+        * inversion Hkind_ϕ; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          specialize (IHHfinst ltac:(auto) eq_refl ltac:(auto) ltac:(auto)).
+          by iApply IHHfinst.
+      + rewrite closure_interp_eq. iEval (cbn -[senv_insert_size]) in "Hcl".
+        pose proof (refresh_kinds_id) as (_ & this & _); try done.
+        apply this in Hkind_ϕ_middle as torew.
+        iDestruct "Hcl" as "#Hcl".
+        iPoseProof (closure_interp_scons_insert_size rti sr mr _ _ with "[$Hcl]") as "Hcl2"; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          done.
+        * inversion Hkind_ϕ; try done.
+        * fold ϕ'0.
+          rewrite <- torew.
+          specialize (IHHfinst ltac:(auto) eq_refl ltac:(auto) ltac:(auto)).
+          by iApply IHHfinst.
+  Qed.
 
   Lemma closure_cant_be_func_host :
-    ∀ F ixs τs1_s τs2_s ϕ se se_cl ft ix,
+    ∀ F ixs τs1_s τs2_s ϕ se ft ix,
       sem_env_interp (Σ:=Σ) F se ->
       function_type_insts F ixs ϕ (InnerFunT (MonoFunT τs1_s τs2_s)) ->
-      closure_interp rti sr ϕ se_cl (FC_func_host ft ix) -∗ False.
+      has_kind_ft F ϕ ->
+      has_kind_ft F (InnerFunT (MonoFunT τs1_s τs2_s)) ->
+      closure_interp rti sr ϕ se (FC_func_host ft ix) -∗ False.
   Proof.
     intros.
     iIntros "#Hcl".
-    pose proof (unravel_closure_interp _ _ _ _ _ se se_cl (FC_func_host ft ix) H H0).
-    iPoseProof (H1 with "[$Hcl]") as "Hcl2".
-    iEval (cbn) in "Hcl2".
-    iDestruct "Hcl2" as "(%a & %b & %c & F)".
-    done.
+    pose proof (unravel_closure_interp _ _ _ _ _ se (FC_func_host ft ix) H H0); try done.
+    iPoseProof (H3 with "[$Hcl]") as "Hcl2"; try done.
   Qed.
+
+  Lemma has_kind_ft_from_ok F τs1 τs2 L :
+    has_instruction_type_ok F (InstrT τs1 τs2) L ->
+    has_kind_ft F (InnerFunT (MonoFunT τs1 τs2)).
+  Proof.
+    (* this should be true by similar logic to first case below *)
+  Admitted.
+
+  (* should be true, but requires an actual fully fleged subsitution lemma most likely *)
+  (* but for has_kind which. not now. *)
+  (* base case is fine though *)
+  Lemma has_kind_ft_from_insts_and_ok F ixs ϕ τs1 τs2 L :
+    function_type_insts F ixs ϕ (InnerFunT (MonoFunT τs1 τs2)) ->
+    has_instruction_type_ok F (InstrT τs1 τs2) L ->
+    has_kind_ft F ϕ.
+  Proof.
+    remember (InnerFunT (MonoFunT τs1 τs2)) as ϕ'.
+    intros Hfinst.
+    induction Hfinst; intros Hok.
+    - subst.
+      constructor.
+      inversion Hok; subst.
+      inversion H; subst.
+      unfold has_mono_rep in *.
+      assert (Forall (λ τ, ∃ κ, has_kind F τ κ) τs1). {
+        apply Forall_lookup_2.
+        intros i τ Hiτ.
+        pose proof (Forall_lookup_1 _ _ _ _ H1 Hiτ).
+        cbn in H3.
+        destruct H3 as (ρ & hrep & hmono).
+        inversion hrep; subst.
+        eexists; done.
+      }
+      (* okay and yes from there, we can get similar things to κs1 and \kappas2  *)
+      (* maybe a weird induction of some lemma on bringing an existential out of a forall *)
+      admit.
+    -
+      subst ϕ''.
+      specialize (IHHfinst eq_refl Hok).
+      rename IHHfinst into Hkind_ϕ'.
+      induction H; subst.
+      + constructor.
+        inversion H; subst.
+        constructor.
+        * (* true by has_kind κ' and subskind_of only changing ref flag *)
+          admit.
+        * (* this seems hard *)
+          admit.
+      + constructor.
+        (* oh crap this is a proper has_kind subst lemma *)
+        (* ugh okay sure that's a future thing *)
+  Admitted.
+
 
   Lemma compat_call M F L wt wt' wtf wl wl' wlf es' i ixs ϕ τs1 τs2 :
     let fe := fe_of_context F in
@@ -84,34 +219,19 @@ Section call.
 
 
     iIntros (??????????) "@@@@@@@@@@".
-
-    (* Note: we are going to be using cwp_call. When I say first/second
-       goal, I'm talking about those. *)
-
-    (* second goal: has_values evs vs. Done by H0 *)
-    (* fifth goal: ↪[frame]fr. Done by "Hfr" *)
-    (* sixth goal: ↪[RUN]. Done by "Hrun"*)
-
-    (* seventh goal: ▷ ?aN ↦[wf] FC_func_native inst (Tf ts1 ts2) ts es
-               this is in instance_interp -> instance_functions_interp
-               then Hϕ says that i,ϕ satisfies, for some i' = a and cl
-                    1. exactly goal 1, but _+i
-                    2. closure_interp .. ϕ cl
-                       I think this will get us ts1, ts2, ts, and es
-                    3. N.of_nat ?a |->[wf] cl, which is seventh goal
-               there will be playing with opening/closing invariants
-     *)
-    (* first goal: f.(f_inst).inst(funcs) (i + funcimm (mr_func_user mr)) = Some a
-       gotten from seventh goal decomposition, Done by Instlookup *)
-    (* fourth goal: N_nat_repr a (N.of_nat a)
-       this is either just fine or will encounter 2^32 issues again *)
-    (* third goal: length evs = length ts1
-       I'll cross this bridge when I have more info (especially with translate_types) *)
-
     (** So, first portion: dig into instance interp as much as possible **)
     iDestruct "Hinst" as "(%HWT & #Hruni & #Hfunci & #Htablei & %Hmm & %Hcg)".
     iPoseProof ((big_sepL_lookup _ _ _ _ Hϕ) with "Hfunci") as "Hϕ".
     iDestruct "Hϕ" as "(%a & %cl & %Instlookup & #Hcl & #nsfun)".
+    iPoseProof (empty_closure_interp rti sr mr se with "[$Hcl]") as "Hcl2".
+    iRename "Hcl" into "HclOLDDDDDDD".
+    iRename "Hcl2" into "Hcl".
+
+    (* kinding quarantine *)
+    pose proof (has_kind_ft_from_insts_and_ok _ _ _ _ _ _ Hfuntype Hok) as Hkind_ϕ.
+    pose proof (has_kind_ft_from_ok _ _ _ _ Hok) as Hkind_mono.
+
+
 
     (* we need cl to be FC_func_native inst ...
        okay yes it will be because of closure_interp.
@@ -123,33 +243,22 @@ Section call.
     iEval (cbn) in "Hcl".
 
 
-
-    (* okay so length evs = length ts1 is going to be weird *)
-    (* it's going to be a weird combo of closure interp to get
-       translate_types se τs1 = Some ts1, that'll use translate_types_sem_interp_length,
-       then atoms_interp_length, then has_values_length
-
-       but I have no idea so how to get the translate_types out of there. It's deep in there
-
-       I'll admit for now
-     *)
-
-    (* the translate types and everything will come out of mono_function_interp
-       the mono_function_interp will come out of unravel_closure_interp
-       however, that's currently not giving us enough info for the length thing.
-       I'll add that later
-     *)
-
-    iPoseProof (unravel_closure_interp _ _ _ _ _ _ _ _ H Hfuntype with "[$Hcl]")
-      as "#Hcl2".
-    iDestruct "Hcl2" as "(%se' & %τs1_s & %τs2_s & #Hcl2 )".
+    (* kinding quarantine portion *)
+    iPoseProof (unravel_closure_interp _ _ _ _ _ _ _ H Hfuntype with "[$Hcl]")
+      as "#Hcl2"; try done.
+    (* iDestruct "Hcl2" as "(%se' & %τs1_s & %τs2_s & #Hcl2 )". *)
+    iDestruct "Hcl2" as "#Hcl2".
     iDestruct "Hcl2" as "(%Htrans1 & %Htrans2 & Hcl2)".
 
-    (* here you can see that we'll need translate_types and some
-       connection between τs1_s and τs1. That will need to happen through
-       unravel_closure_interp being better
-     *)
-
+    iAssert (⌜length evs = length ts1⌝%I) as "%hlenevsts1". {
+      iPoseProof (translate_types_sem_interp_length with "[$Hos]") as "%len1"; try done.
+      rewrite <- len1.
+      iPoseProof (big_sepL2_length with "[$Hvs]") as "%len2"; try done.
+      rewrite len2.
+      iPureIntro.
+      unfold has_values in H0.
+      apply has_values_length. done.
+    }
 
     (* before applying, open invariant *)
     iApply fupd_cwp.
@@ -161,7 +270,6 @@ Section call.
     - rewrite Nat.add_comm.
       done.
     - done.
-    - admit. (* here's the one *)
     - done.
     - iModIntro.
       iIntros "Hfr Hrun nsfun1".
@@ -173,10 +281,7 @@ Section call.
 
       iPoseProof ("Hcl2" with "[$Hvs] [Hos] [$Hrt] [$Hown] [$Hfr] [$Hrun]") as "Hcwp".
       {
-        (* this will rely on more connections between se<->se' and τs1<->τs1_s
-           this will also have to be added/gotten from unravel_closure_interp
-         *)
-        admit.
+        done.
       }
 
       iApply (cwp_frame_ctx1 with "[Hcwp] [Hframe]").
@@ -184,25 +289,22 @@ Section call.
         { iApply "Hframe". }
         { iIntros (??) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)". iFrame.
           iSplitR; auto.
-          (* here's the necessary τs2<->τs2_s connection *)
-          admit.
         }
         { iIntros (?) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)". iFrame.
           iSplitR; auto.
-          (* same as above *)
-          admit.
         }
         {
           iIntros (f vs0) "Hframe ((%os2 & Hvs2 & Hos2) & [%θ' Hrt] & Hown)".
+          iAssert (⌜length vs0 = length ts2⌝%I) as "%lenvs0ts2". {
+            change (values_interp1 (map (type_interp rti sr) τs2) se os2) with
+              (values_interp rti sr se τs2 os2).
+            iPoseProof (translate_types_sem_interp_length with "[$Hos2]") as "%len1"; try done.
+            rewrite <- len1.
+            iPoseProof (big_sepL2_length with "[$Hvs2]") as "%len2"; try done.
+          }
           iFrame.
-          (* here we need same as above, but also the same reasoning as for the
-             previous length based goal but between vs0?
-             ah yes this will similarly go through translate_types/whatever added onto
-             unravel_closure_interp and atoms_interp
-             so same as above but slightly more involved
-           *)
-          admit.
+          iSplitR; try done.
         }
-  Admitted.
+  Qed.
 
 End call.
